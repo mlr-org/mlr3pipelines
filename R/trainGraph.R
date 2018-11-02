7686a95b23a238ece540a0259816cd076fe36790
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-11-02 09:34:05 +0100

    terminate training when there's not operations left in op$next_ops

--- R/trainGraph.R
+++ R/trainGraph.R
@@ -5,22 +5,28 @@ trainGraph = function(root, task) {
   front = OpList$new(list(root))
 
   while(length(front) > 0L) {
-    messagef("front step, front=%s", front$print_str)
+    BBmisc::messagef("front step, front=%s", front$print_str)
     new_front = OpList$new()
     to_remove = integer(0L)
     for (i in seq_along(front)) {
       op = front[[i]]
       op$acquire_inputs()
-      messagef("checking front node %s, can_fire=%s", op$id, op$can_fire)
+      BBmisc::messagef("checking front node %s, can_fire=%s", op$id, op$can_fire)
       if (op$can_fire) {
         op$train()
+        
+        if(is.null(op$next_ops)) {
+          # there's no next operations
+          # so the loop can be stopped
+          break
+        }
         new_front$join_new(op$next_ops)
       } else {
         new_front$add(op) 
       }
     }
     front = new_front
-    messagef("front step done, front=%s", front$print_str)
+    BBmisc::messagef("front step done, front=%s", front$print_str)
   }
 }
 
