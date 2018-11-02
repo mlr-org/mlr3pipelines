17da2f64ed1ee8d0fae3ba7dcdd511ef052bb6f4
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-11-02 13:16:06 +0100

    PipeOpFeatureUnion - add basic support for DataTableBackend

--- tests/testthat/test_featureunion.R
+++ tests/testthat/test_featureunion.R
@@ -1,33 +1,38 @@
-# load_all("~/cos/paradox")
-# load_all("~/cos/mlr3")
-load_all()
-
-task = mlr_tasks$get("iris")
-dd = iris[, -5]
-nd = iris[, -5]
-
-op1 = PipeOpScaler$new()
-
-op2 = PipeOpFanOut$new(2)
-
-op3a = PipeOpPCA$new()
-
-op3b = PipeOpNULL$new()
-
-op4 = PipeOpFeatureUnion$new()
-
-lrn = mlr_learners$get("classif.rpart")
-op5 = PipeOpLearner$new(learner = lrn)
-
-op1$set_next(list(op2))
-
-op2$set_next(list(op3a, op3b))
-
-op4$set_prev(list(op3a, op3b))
-
-op4$set_next(list(op5))
-
-trainGraph(op1, task)
-
-
+test_that("featureunion - basic", {
+  
+  set.seed(123)
+  
+  task = mlr_tasks$get("iris")
+  dd = iris[, -5]
+  nd = iris[, -5]
+  
+  op1 = PipeOpScaler$new()
+  
+  op2a = PipeOpPCA$new()
+  op2b = PipeOpNULL$new()
+  
+  op3 = PipeOpFeatureUnion$new()
+  
+  lrn = mlr_learners$get("classif.rpart")
+  op4 = PipeOpLearner$new(learner = lrn)
+  
+  op1$set_next(list(op2a, op2b))
+  
+  op2a$set_next(list(op3))
+  op2b$set_next(list(op3))
+  
+  op3$set_next(list(op4))
+  
+  trainGraph(op1, task)
+  
+  model <- op4$params$model
+  
+  paramNamesFeatureUnion <- c(
+    "OpNULL.Petal.Width", "OpNULL.Petal.Length", "pca.Sepal.Length", 
+    "OpNULL.Sepal.Length", "pca.Sepal.Width", "OpNULL.Sepal.Width", 
+    "pca.Petal.Length")
+  
+  expect_equal(names(model$variable.importance), paramNamesFeatureUnion)
+  
+})
 
