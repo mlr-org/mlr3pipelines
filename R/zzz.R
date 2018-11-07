122bd0124dbacc5e10134c434f933b8599eba1b2
Michel Lang <michellang@gmail.com>
2018-11-07 15:14:20 +0100

    cleanup dependencies

--- R/zzz.R
+++ R/zzz.R
@@ -4,9 +4,7 @@
 #' @import mlr3
 #' @import paradox
 #' @importFrom stats setNames predict
-#' @importFrom utils data
 #' @importFrom BBmisc vlapply viapply vcapply vnapply seq_row seq_col isFALSE
-#' @importFrom glue glue collapse single_quote
 #' @importFrom R6 R6Class
 #' @keywords internal
 "_PACKAGE"
