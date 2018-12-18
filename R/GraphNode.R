c992ebbc350fbcae8f46f410bf558d81a51c73fe
mb706 <mlr.developer@mb706.com>
2018-12-18 13:40:49 +0100

    GraphNode.R cleaned up

--- R/GraphNode.R
+++ R/GraphNode.R
@@ -1,10 +1,10 @@
 
 GraphElement = R6::R6Class("GraphElement",
   active = list(
-      intype = function() stop("abstract"),
-      outtype = function() stop("abstract"),
-      inputs = function() stop("abstract"),
-      outputs = function() stop("abstract")
+      intype = function() stop("abstract"),  # list of character
+      outtype = function() stop("abstract"),  # list of character
+      inputs = function() stop("abstract"),  # list of GraphEdge
+      outputs = function() stop("abstract")  # list of GraphEdge
   )
 )
 
@@ -18,8 +18,6 @@ GraphEdge = R6::R6Class("GraphEdge",
       }
   )
 )
-
-
 
 #### Class definition ####
 
@@ -36,58 +34,86 @@ GraphEdge = R6::R6Class("GraphEdge",
 #' @importFrom R6 R6Class
 #'
 #'
-GraphNode = R6::R6Class(
-  "GraphNode",
-  public = list(
-    initialize = function(pipeop) {
-      self$pipeop = pipeop
-      self$next_nodes = GraphNodesList$new()
-      self$prev_nodes = GraphNodesList$new()
-    },
-    pipeop = NULL,
-    inputs = list(),
+GraphNode = R6::R6Class("GraphNode",
+  inherits = GraphElement,
 
-    # next, prev
+  public = list(
+    pipeop = NULL,
     next_nodes = NULL,
     prev_nodes = NULL,
-    set_next = graph_node_set_next,
-    set_prev = graph_node_set_prev,
-    add_next = graph_node_add_next,
-    add_prev = graph_node_add_prev,
-    train = function() {
-      if (self$has_lhs) {
-        self$inputs = self$prev_nodes$map(function(x) x$result)
-      }
-      BBmisc::messagef("Train op='%s'", self$id)
-      self$pipeop$train(self$inputs)
+    initialize = function(pipeop, graph) {
+      self$pipeop = pipeop
+      self$graph = graph
+      self$.next_nodes = sapply(pipeop$intype, function(.) NULL, simplify = FALSE)
+      self$.prev_nodes = sapply(pipeop$outtype, function(.) NULL, simplify = FALSE)
+      self$.in_edges = sapply(names(pipeop$intype), GraphEdge$new, element = self, simplify = FALSE)
+      self$.out_edges = sapply(names(pipeop$outtype), GraphEdge$new, element = self, simplify = FALSE)
     },
-    next_node = function(id = 1) self$next_nodes[[id]],
 
     print = function(...) {
-      BBmisc::catf("GraphNode: <%s>", self$id)
+      BBmisc::catf("GraphNode: <%s>", self$pipeop$id)
     }
   ),
-  private = list(),
+  private = list(
+      .next_nodes = NULL,
+      .prev_nodes = NULL,
+      .editlock = FALSE,
+      .in_edges = NULL,
+      .out_edges = NULL,
+      .graph = NULL
+  ),
   active = list(
+      graph = function() private$.graph
+      prev_nodes = function(prev) {
+        if (missing(prev)) {
+          private$.prev_nodes
+        } else {rev
+          connectgn(prev, ".prev_nodes", private)
+        }
+      },
+      next_nodes = function(nxt) {
+        if (missing(nxt)) {
+          private$.next_nodes
+        } else {
+          connectgn(nxt, ".next_nodes", private)
+        }
+      },
+      in_edges = function() private$.in_edges,
+      out_edges = function() private$.out_edges,
 
-    # forwarded
-    id = function() self$pipeop$id,
-    result = function() self$pipeop$result,
-    has_result = function() !is.null(self$pipeop$result),
-    is_learnt  = function() self$pipeop$is_learnt,
-
-    has_lhs = function() length(self$prev_nodes) > 0L,
-    has_rhs = function() length(self$next_nodes) > 0L,
-    can_fire = function() {
-      if (!self$has_lhs) length(self$inputs) > 0
-      else all(self$prev_nodes$map_s(function(x) x$has_result))
-    },
-    root_node = function() {
-      if(self$has_no_prevs) return(self)
-      self$prev_nodes[[1]]$root_node
-    }
+    input_complete = function() !any(sapply(self$prev_nodes, is.null)),
+    output_complete = function() !any(sapply(self$next_nodes, is.null))
   )
 )
+
+connectgn = function(newnodes, oldnodename, private) {
+  # TODO: assert prev is a list
+  if (!identical(names(newnodes), names(private[[oldnodename]]))) {
+    stop("Can't change names of nodes")
+  }
+  if (private$.editlock) return(NULL)
+  backup = private[[oldnodename]]
+  on.exit({private$.editlock = FALSE ; private[[oldnodename]] = backup})
+  private$.editlock = TRUE
+  for (idx in seq_along(newnodes)) {
+    if (!identical(newnodes[[idx]]$element$graph, private$.graph)) {
+      stop("Can't connect nodes that are not in the same graph")
+    }
+    oldnode = private[[oldnodename]][[idx]]
+    edgename = names(newnodes)[[idx]]
+    if (edgename == "") edgename = idx
+    #TODO: assert class prev[[idx]]: NULL or GraphEdge
+    if (identical(newnodes[[idx]], oldnode)) {
+      next
+    }
+    oldnode$element$next_nodes[[oldnode$name]] = NULL
+    newnodes[[idx]]$element$next_nodes[[newnodes[[idx]]$name]] = GraphEdge(edgename, self)
+  }
+  private[[oldnodename]] = newnodes
+  private$.editlock = FALSE
+  on.exit()
+  # TODO: update graph in / out nodes
+}
 
 wrap_nodes = function(x) {
 
