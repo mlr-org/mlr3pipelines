14265e425eab63635f562e8af5072f85e2e6ada4
mb706 <mlr.developer@mb706.com>
2018-12-20 18:04:30 +0100

    reenable featureunion test

--- tests/testthat/test_featureunion.R
+++ tests/testthat/test_featureunion.R
@@ -1,40 +1,35 @@
-# context("featureunion")
-# test_that("featureunion - basic", {
+context("featureunion")
+test_that("featureunion - basic", {
 
-#   task = mlr_tasks$get("iris")
-#   dd = iris[, -5]
-#   nd = iris[, -5]
+  task = mlr_tasks$get("iris")
+  dd = iris[, -5]
+  nd = iris[, -5]
 
-#   op1 = PipeOpScale$new()
+  op1 = PipeOpScale$new()
 
-#   op2a = PipeOpPCA$new()
-#   op2b = PipeOpNULL$new()
+  op2a = PipeOpPCA$new()
+  op2b = PipeOpNULL$new()
 
-#   op3 = PipeOpFeatureUnion$new()
+  op3 = PipeOpFeatureUnion$new(2)
 
-#   lrn = mlr_learners$get("classif.rpart")
-#   op4 = PipeOpLearner$new(learner = lrn)
+  lrn = mlr_learners$get("classif.rpart")
+  op4 = PipeOpLearner$new(learner = lrn)
 
-#   PipeOpCopy$new(2)
+  scatter = PipeOpCopy$new(2)
+
+  graph = scatter %>>% gunion(op2a, op2b) %>>% op3
+  graph$plot()
+
+  trained = graph$train(task)
+
+  graph[["pca"]]$pipeop$state
 
 
-#   root = GraphNode$new(op1)
-#   root$
-#     set_next(list(GraphNode$new(op2a), GraphNode$new(op2b)))$
-#     set_next(GraphNode$new(op3))$
-#     set_next(GraphNode$new(op4))
 
-#   graph = Graph$new(root)
-#   graph$train(task)
+  param_names_union = c(
+    "OpNULL.Petal.Width", "OpNULL.Petal.Length",
+    "pca.PC1", "OpNULL.Sepal.Length", "pca.PC2",
+    "OpNULL.Sepal.Width", "pca.PC3")
 
-#   model = op4$params$model
-
-#   param_names_union = c(
-#     "OpNULL.Petal.Width", "OpNULL.Petal.Length",
-#     "pca.PC1", "OpNULL.Sepal.Length", "pca.PC2",
-#     "OpNULL.Sepal.Width", "pca.PC3")
-
-#   expect_equal(names(model$variable.importance), param_names_union)
-
-# })
+})
 
