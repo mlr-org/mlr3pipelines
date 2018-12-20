2efbbc126180e3395a7255a0436743857cfc381e
mb706 <mlr.developer@mb706.com>
2018-12-20 13:20:46 +0100

    PipeOpCopy with default id

--- R/PipeOpCopy.R
+++ R/PipeOpCopy.R
@@ -4,7 +4,7 @@
 PipeOpCopy = R6::R6Class("PipeOpCopy",
   inherit = PipeOp,
   public = list(
-    initialize = function(id, outnum) {
+    initialize = function(outnum, id = "scatter") {
       super$initialize(id)
       private$.intype = list("any")
       private$.outtype = rep(list("any"), outnum)
