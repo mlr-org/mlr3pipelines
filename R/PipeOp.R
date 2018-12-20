a51b71da1ae2545e813d8fdc46f31f7b7785d13a
mb706 <mlr.developer@mb706.com>
2018-12-20 11:26:23 +0100

    graph param_set

--- R/PipeOp.R
+++ R/PipeOp.R
@@ -7,12 +7,12 @@
 #'   This class is the baseclass for all pipeOp's, and is not intended for direct use.
 #'   During training it takes inputs, tranforms them, while doing that learns and stores its
 #'   parameters and then returns the output.
-#'   During prediction it applies the learned params to the input and transforms the input to an output.' 
+#'   During prediction it applies the learned params to the input and transforms the input to an output.'
 #'
 #'   A PipeOp specifies the types of inputs and outputs as `intype` and `outtype`, a list of <something specifying types>.
 #'   The length of these lists determines the length of input / output the PipeOp produces. Typically the PipeOp input / output
 #'   is a list of specified length, but PipeOps with input / output length 1 can specify that they don't use lists but use singular
-#'   values instead (`.takeslist` / `.returnslist` set to `FALSE`)#' 
+#'   values instead (`.takeslist` / `.returnslist` set to `FALSE`)#'
 #'
 #' @section Usage:
 #' * `f = pipeOp$new(id, params)` \cr
@@ -24,7 +24,7 @@
 #' * `f$is_trained` -> `logical(1)`
 #' * `f$params` -> `any`
 #' * `f$result` -> `any`
-#' * `f$intype` -> `list of any` 
+#' * `f$intype` -> `list of any`
 #' * `f$outtype` -> `list of any`
 #' * `f$takeslist` -> `logical(1)`
 #' * `f$returnslist` -> `logical(1)`
@@ -33,18 +33,18 @@
 #'   `any` -> `any`
 #' * `f$predict()` \cr
 #'   `any` -> `any`
-#' 
+#'
 #' @section Details:
 #' * `new()`: Constructs the pipeOp from an id string and a (possibly empty) [ParamSet].
 #' * `id`: Active binding that allows to return and set the id of the PipeOps. Ids are user-configurable, and ids of PipeOps in graphs must be unique.
-#' * `packages`: Packages required for the pipeOp. 
+#' * `packages`: Packages required for the pipeOp.
 #' * `param_set`: The set of all exposed parameters of the PipeOp.
 #' * `par_vals`: A named list of parameter settings where all setting must come from `param_set`.
 #' * `is_trained`: Is the PipeOp currently trained?
 #' * `params`: The object of learned parameters, obtained in the training step, and applied in the predict step.
 #' * `result`: A slot to store the result of either the `train` or the `predict` step, after it was
 #' *   applied.
-#' * `print()`: Prints 
+#' * `print()`: Prints
 #' * `train()`: Function that is responsible to train on `input`, transform it to output and store the learned `params`.
 #' *   If the PipeOp is already trained, already present `params` are overwritten.
 #' * `predict()`: Function that is responsible to predict on `input`, and transform it to output by applying the learned `params`.
@@ -55,13 +55,13 @@
 #'   If this is `FALSE`, `length(.intype)` must be 1.  Read-only.
 #' * `returnslist`: `TRUE` if output of `train` / `predict` is a list, `FALSE` if it is a singular value.
 #'   If this is `FALSE`, `length(.outtype)` must be 1.  Read-only.
-#' 
+#'
 #' @section Internals:
-#' * `.intype`: `list of any` 
+#' * `.intype`: `list of any`
 #' * `.outtype`: `list of any`
 #' * `.takeslist`: `logical(1)`
-#' * `.returnslist`: `logical(1)`' 
-#' 
+#' * `.returnslist`: `logical(1)`'
+#'
 #' @name pipeOp
 #' @family pipeOp
 PipeOp = R6::R6Class("PipeOp",
@@ -138,6 +138,6 @@ PipeOp = R6::R6Class("PipeOp",
     .intype = NULL,  # list of character vectors, identifying the input classes
     .outtype = NULL,  # list of character vectors, identifying output classes
     .takeslist = TRUE,  # may be FALSE, but only if length(intype) is 1
-    .returnslist = TRUE,  # may be FALSE, but only if length(outtype) is 1
+    .returnslist = TRUE  # may be FALSE, but only if length(outtype) is 1
   )
 )
