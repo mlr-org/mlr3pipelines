4f6a4698355d620f854d304eab3399e5fa007b85
mb706 <mlr.developer@mb706.com>
2018-12-20 13:17:53 +0100

    PipeOpCopy added

---
+++ R/PipeOpCopy.R
@@ -0,0 +1,27 @@
+
+
+
+PipeOpCopy = R6::R6Class("PipeOpCopy",
+  inherit = PipeOp,
+  public = list(
+    initialize = function(id, outnum) {
+      super$initialize(id)
+      private$.intype = list("any")
+      private$.outtype = rep(list("any"), outnum)
+      private$.outnum = outnum
+    },
+    train = function(input) {
+      self$state = list()
+      rep(input, self$outnum)
+    },
+    predict = function(input) {
+      rep(input, self$outnum)
+    }
+  ),
+  private = list(
+    .outnum = NULL
+  ),
+  active = list(
+    outnum = function() private$.outnum
+  )
+)
