45abd13adf73f6a125541400390e9d16bf4b2b2a
pfistfl <pfistererf@googlemail.com>
2019-01-07 12:55:13 +0100

    Fix paradox changesQ

--- R/PipeOp.R
+++ R/PipeOp.R
@@ -61,8 +61,7 @@ PipeOp = R6::R6Class("PipeOp",
       private$.id = id
       private$.param_set = param_set
       #FIXME: we really need a function in paradox now to get defaults
-      private$.param_vals = param_set$data$default
-      names(private$.param_vals) = param_set$ids
+      private$.param_vals = param_set$defaults
       private$.param_vals = insert_named(private$.param_vals, param_vals)
       if (!param_set$test(private$.param_vals)) {
         stop("Parameters out of bounds")
