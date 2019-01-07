7b57b2d0317132d3aa30210750e07e4cfa0f555f
pfistfl <pfistererf@googlemail.com>
2019-01-07 16:10:18 +0100

    Featureunion usecase

---
+++ tests/testthat/test_usecase_featureunion.R
@@ -0,0 +1,26 @@
+test_that("Usecase - Featureunion", {
+
+  # Define PipeOp's
+  op1 = PipeOpScale$new()
+  scatter = PipeOpCopy$new(2)
+  op2a = PipeOpPCA$new()
+  op2b = PipeOpNULL$new()
+  op3 = PipeOpFeatureUnion$new(2)
+
+  # mlr3 Objects
+  task = mlr_tasks$get("iris")
+  lrn = mlr_learners$get("classif.rpart")
+  op4 = PipeOpLearner$new(learner = lrn)
+
+  # Construct graph
+  graph = op1 %>>% scatter %>>% gunion(op2a, op2b) %>>% op3 %>>% op4
+  expect_false(graph$is_trained)
+  test_basic_graph_props(graph)
+  expect_true(length(graph) == 6L)
+
+  # Train and predict
+  graph$train(task)
+  expect_class(graph[["classif.rpart"]]$pipeop$state, "Experiment")
+  # graph$predict(task)
+})
+
