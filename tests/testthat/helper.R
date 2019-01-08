c5e9063ed17a2ccfe1169def812d740898a4f87a
Michel Lang <michellang@gmail.com>
2019-01-08 17:42:40 +0100

    tests

---
+++ tests/testthat/helper.R
@@ -0,0 +1,2 @@
+lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
+
--- tests/testthat/test_graphdt.R
+++ tests/testthat/test_graphdt.R
@@ -8,13 +8,16 @@ test_that("linear graph", {
   op_lrn = PipeOpLearner$new(mlr_learners$get("classif.rpart"))
   g$add_pipeop(op_ds)
   g$add_pipeop(op_pca)
-  g$add_pipeop(op_lrn)
+  # g$add_pipeop(op_lrn)
   g$add_channel("downsample", "1", "pca", "1")
-  g$add_channel("pca", "1", "classif.rpart", "1")
+  # g$add_channel("pca", "1", "classif.rpart", "1")
 
   expect_graph(g)
 
   input = mlr_tasks$mget("iris")
-  g$fire(input, "train")
-  g$fire(input, "predict")
+  x = g$fire(input, "train")
+  expect_task(x[[1]])
+
+  out = g$fire(input, "predict")
+  expect_task(x[[1]])
 })
