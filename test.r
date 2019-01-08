6007a4f22800464eb6796fd1ca51cf9d68dc25cc
Michel Lang <michellang@gmail.com>
2019-01-08 19:59:41 +0100

    test file

---
+++ test.r
@@ -0,0 +1,45 @@
+if (FALSE) {
+op1 = PipeOpDownsample$new()
+op2 = PipeOpLearner$new(mlr_learners$get("classif.rpart"))
+
+g1 = ensure_graph(op1)
+g2 = ensure_graph(op2)
+
+g = op1 %>>% op2
+g$plot()
+g$channels
+g$pipeops
+
+x = greplicate(graph, 2)
+x$channels
+x$plot()
+
+}
+
+
+g = gunion(list(PipeOpScale$new(), PipeOpPCA$new())) %>>%
+  PipeOpFeatureUnion$new(2)
+
+g$plot()
+
+g = g %>>% PipeOpLearner$new(mlr_learners$get("classif.rpart"))
+task = mlr_tasks$get("iris")
+g$fire(list(task = task), "train")
+g$pipeops$classif.rpart$state$model
+g$fire(list(task = task), "predict")
+
+
+g = PipeOpChunk$new(3) %>>% PipeOpLearner$new(mlr_learners$get("classif.rpart"))
+
+g$plot()
+
+g = g %>>% PipeOpLearner$new(mlr_learners$get("classif.rpart"))
+task = mlr_tasks$get("iris")
+g$fire(list(task = task), "train")
+g$pipeops$classif.rpart$state$model
+g$fire(list(task = task), "predict")
+
+
+g = PipeOpChunk$new(3) %>>% PipeOpLearner$new(mlr_learners$get("classif.rpart"))
+g$plot()
+g$channels
