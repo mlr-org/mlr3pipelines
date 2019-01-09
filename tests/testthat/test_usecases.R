a58d0239cca2bbed0f3e3874e70fe5dd83c98a56
Bernd Bischl <bernd_bischl@gmx.net>
2019-01-09 15:22:13 +0100

    usecase tests

--- tests/testthat/test_usecases.R
+++ tests/testthat/test_usecases.R
@@ -1,3 +1,5 @@
+#FIXME: pipeops shouldnt have an ID in their constructor. the ID should be an AB.
+
 context("usecases for pipelines")
 
 test_that("linear: scale + pca", {
@@ -19,7 +21,7 @@ test_that("featureunion", {
 
   g = gunion(list(PipeOpPCA$new(), PipeOpNULL$new())) %>>% PipeOpFeatureUnion$new(2L) %>>% PipeOpLearner$new(learner = lrn)
   expect_false(g$is_trained)
-  expect_graph(g)
+  expect_graph(g, n_nodes = 4L, n_edges = 3L)
 
   g$train(task)
   # # FIXME: also do a predict
@@ -33,15 +35,36 @@ test_that("bagging", {
   task = mlr_tasks$get("iris")
   lrn = mlr_learners$get("classif.rpart")
 
-  # FIXME: modelavg is not there!
-  # g = greplicate(PipeOpDownsample$new() %>>% PipeOpLearner$new(lrn), 2L) %>>% PipeOpModelAvg()
-  # expect_graph(g)
+  g = greplicate(PipeOpDownsample$new() %>>% PipeOpLearner$new(lrn), 2L) %>>%
+    PipeOpModelAvg$new(innum = 2L)
+  expect_graph(g, n_nodes = 5L, n_edges = 4L)
 
-  # g$train(list(task))
-  # # FIXME: also do a predict
-  # # graph$predict(task)
-  # FIXME: at least roughly check that correct stuff is returned
+  g$train(task)
+  g$predict(task)
 })
+
+
+test_that("branching", {
+  task = mlr_tasks$get("iris")
+  lrn1 = mlr_learners$get("classif.rpart")
+  lrn2 = mlr_learners$get("classif.featureless")
+
+  # FIXME: are we REALLY sure that stuff here gets connected in the correct order?
+  # i doubt that and this looks really bad and errorprone
+  # b) we really want to have an associated order in the graph which is determined by
+  # the nodes in the pipeops, not lexical by id or something else
+  # c) we need a delayed init of the pipeop, which is triggered when the pipeop is
+  # connected. this  would help a lot when we connect the PipeOp to its neighbor and
+  # need some info from the nieghbor to decide some poperties of the PO
+  g = PipeOpBranch$new(2L) %>>%
+    gunion(list(PipeOpLearner$new(lrn1), PipeOpLearner$new(lrn2))) %>>%
+    PipeOpUnbranch$new(2L)
+  # expect_graph(g, n_nodes = 5L, n_edges = 4L)
+   #FIXME: finish test
+  # g$train(list(task))
+  # graph$predict(task)
+})
+
 
 
 
