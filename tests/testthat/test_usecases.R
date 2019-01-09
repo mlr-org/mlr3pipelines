244ef4ac0b79bd90258f92661ec8882d78e3cd1b
Bernd Bischl <bernd_bischl@gmx.net>
2019-01-09 14:10:56 +0100

    ...

--- tests/testthat/test_usecases.R
+++ tests/testthat/test_usecases.R
@@ -5,10 +5,9 @@ test_that("linear: scale + pca", {
   task = mlr_tasks$get("iris")
   g = PipeOpScale$new() %>>% PipeOpPCA$new()
   expect_graph(g)
-  # FIXME: we should probably not have a list imte
-  res1 = g$train(list(task))
+  res1 = g$train(task)
   assert_list(res1, types = "Task")
-  res2 = g$predict(list(task))
+  res2 = g$predict(task)
   assert_list(res2, types = "Task")
   expect_equal(res1[[1]]$data(), res2[[1]]$data())
 })
@@ -22,11 +21,30 @@ test_that("featureunion", {
   expect_false(g$is_trained)
   expect_graph(g)
 
-  g$train(list(task))
+  g$train(task)
   # # FIXME: also do a predict
   # # graph$predict(task)
   # FIXME: at least roughly check that correct stuff is returned
 })
+
+#FIXME: have a look at intermediate results in all usecase, we should expect some stuff there
+
+test_that("bagging", {
+  task = mlr_tasks$get("iris")
+  lrn = mlr_learners$get("classif.rpart")
+
+  # FIXME: modelavg is not there!
+  # g = greplicate(PipeOpDownsample$new() %>>% PipeOpLearner$new(lrn), 2L) %>>% PipeOpModelAvg()
+  # expect_graph(g)
+
+  # g$train(list(task))
+  # # FIXME: also do a predict
+  # # graph$predict(task)
+  # FIXME: at least roughly check that correct stuff is returned
+})
+
+
+
 
 
 
