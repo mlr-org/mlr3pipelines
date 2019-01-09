920a8c2e3b62808383269f3891e42cd583cbdd5f
mb706 <mlr.developer@mb706.com>
2019-01-09 15:54:27 +0100

    docstring fix

--- R/gunion.R
+++ R/gunion.R
@@ -1,14 +1,13 @@
 #' @title Union of graphs
 #'
 #' @description
-#' Takes an arbitrary amount of Graphs, GraphNodes and PipeOps as inputs and joins
+#' Takes an arbitrary amount of [Graph]s and/or [PipeOp]s as inputs and joins
 #' them by "stacking" them one over the other.
 #' Returns the full graph.
 #'
-#' @param ... A list of [Graph], [GraphNode] or [PipeOp] \cr
-#' List of elements with one of the types defined above.
-#' @param .graphs list of [Graph]` \cr
-#'   Graphs which are to be joined.
+#' @param graphs A list of [Graph] or [PipeOp] \cr
+#'   List of elements with one of the types defined above, which are the
+#'   graphs to be joined.
 #' @return `[Graph]`
 gunion = function(graphs) {
   graphs = map(graphs, ensure_graph)
@@ -17,7 +16,7 @@ gunion = function(graphs) {
   g$pipeops = unlist(map(graphs, "pipeops"), recursive = FALSE)
   assert_names(names(g$pipeops), type = "unique", .var.name = "ids of pipe operators")
   g$pipeops = map(g$pipeops, function(x) x$clone(deep = TRUE))
-  g$edges = rbindlist(map(graphs, "edges"))
+  g$edges = rbindlist(maPip(graphs, "edges"))
   g
 }
 
