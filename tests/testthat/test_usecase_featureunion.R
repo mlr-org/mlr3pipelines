c33f754124cfe3468ad1b3114129ee9ad5165d36
pfistfl <pfistererf@googlemail.com>
2019-01-09 10:51:44 +0100

    Fix pipeops

--- tests/testthat/test_usecase_featureunion.R
+++ tests/testthat/test_usecase_featureunion.R
@@ -1,4 +1,8 @@
 context("Usecase - Featureunion")
+
+
+
+
 test_that("Usecase - Featureunion", {
 
   # Define PipeOp's
@@ -16,11 +20,11 @@ test_that("Usecase - Featureunion", {
   # Construct graph
   graph = op1 %>>% scatter %>>% gunion(list(op2a, op2b)) %>>% op3 %>>% op4
   expect_false(graph$is_trained)
-  test_basic_graph_props(graph)
+  expect_graph(graph)
 
   # Train and predict
   # graph$train(list(task))
-  expect_class(graph[["classif.rpart"]]$pipeop$state, "Experiment")
+  # expect_class(graph[["classif.rpart"]]$pipeop$state, "Experiment")
   # graph$predict(task)
 })
 
