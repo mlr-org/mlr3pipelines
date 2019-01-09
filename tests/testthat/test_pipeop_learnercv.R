5628cda821fa073a1f8a423c42fefa79bdbe40a7
pfistfl <pfistererf@googlemail.com>
2019-01-09 18:14:42 +0100

    Update tests

--- tests/testthat/test_pipeop_learnercv.R
+++ tests/testthat/test_pipeop_learnercv.R
@@ -8,8 +8,14 @@ test_that("PipeOLearnerCV - basic properties", {
   expect_data_table(po$output, nrow = 1)
 
   task = mlr_tasks$get("iris")
-  result = train_pipeop(po, list(task = task))
-  expect_class(result[[1L]], "Task")
+  tsk = train_pipeop(po, list(task = task))[[1]]
+  expect_class(tsk, "Task")
+  expect_true(tsk$nrow == 150L)
+  expect_true(tsk$ncol == 2L)
+  expect_true(task$target_names == tsk$target_names)
+  expect_true(task$class_names == tsk$class_names)
+  vals = factor(unique(tsk$data(col = tsk$feature_names)$response))
+  expect_equivalent(vals, task$class_names)
 
   result = predict_pipeop(po, list(task = task))
   expect_class(result[[1L]], "Prediction")
