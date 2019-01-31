1dfa318809e583349a08f1c770baa58813829a71
mb706 <mlr.developer@mb706.com>
2019-01-31 13:06:08 +0100

    document %>>% operator

--- R/operators.R
+++ R/operators.R
@@ -1,25 +1,33 @@
-########## Basic operator %>>% for 1-to-1, 1-to-n, n-to-1 ##########
-
-
-# FIXME: getter for length of channels
-# FIXME: lhs and rhs must be the number of pipeops,  and for each pair the number of channels must be equal
-
-# we nneed to DOCUMENT semantics of the >> op, wrt to what happens with multiple rhs / lhs channels
-
-# we need to simply document a few cases where >> works. it does not need to in totally arbitrary cases, but robustly in the cases it
-# asserts
-
 #' @title PipeOp Composition Operator
 #'
 #' @description
 #' This operator \dQuote{pipes} data from the source `g1` into the sink `g2`. Both source and sink can either be
-#' a [Graph] or a [PipeOp]. The number of output channels of `g1` must equal the number of input channels
-#' of the `g2`.
+#' a [`Graph`] or a [`PipeOp`]. The number of output channels of `g1` (as given by `g1$output`) must equal the
+#' number of input channels of `g2` (as given by `g2$input`). Connections between channels are created in the
+#' order in which they occur in `g1` and `g2`, respectively: `g1`'s output channel 1 is connected to `g2`'s input
+#' channel 1, channel 2 to 2 etc.
+#'
+#' This operator always created deep copies of its input arguments, so they cannot be modified by reference.
+#' To access individual `PipeOp`s after composition, use the resulting `Graph`'s `$pipeops` list.
+#'
 #'
 #' @param g1 ([`Graph`] | [`PipeOp`]) \cr
-#'   [Graph] / [PipeOp] to put in front of `g2`.
+#'   `Graph` / `PipeOp` to put in front of `g2`.
 #' @param g2 ([`Graph`] | [`PipeOp`]) \cr
-#'   [Graph] / [PipeOp] to put after  `g1`.
+#'   `Graph` / `PipeOp` to put after  `g1`.
+#'
+#' @examples
+#' o1 = PipeOpScale$new()
+#' o2 = PipeOpPCA$new()
+#'
+#' # The following two are equivalent:
+#' result1 = o1 %>>% o2
+#'
+#' result2 = Graph$new()$
+#'   add_pipeop(o1$copy(deep = TRUE))$
+#'   add_pipeop(o2$copy(deep = TRUE))$
+#'   add_edge(o1$id, o2$id)
+#' @family mlr3pipelines backend related
 #' @export
 `%>>%` = function(g1, g2) {
   g1 = ensure_graph(g1)
@@ -34,9 +42,6 @@
   # build edges from free output channels of g1 and free input channels of g2
   new_edges = cbind(g1out[, list(src_id = get("op.id"), src_channel = get("channel.name"))],
     g2in[, list(dst_id = get("op.id"), dst_channel = get("channel.name"))])
-
   g$edges = rbind(g$edges, new_edges)
-
-
   g
 }
