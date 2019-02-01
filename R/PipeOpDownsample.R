0b2f47e40b9509c293459dad6b8f006e9c8f7266
mb706 <mlr.developer@mb706.com>
2019-02-01 19:55:57 +0100

    rename PipeOpDownsample -> PipeOpSubsample

--- R/PipeOpDownsample.R
+++ R/PipeOpDownsample.R
@@ -1,44 +1,48 @@
-#' @title PipeOpDownsample
+#' @title PipeOpSample
 #'
-#' @name mlr_pipeop_downsample
+#' @name mlr_pipeop_subsample
 #' @format [`R6Class`] object inheriting from [`PipeOpTaskPreproc`].
 #'
 #' @description
-#'   Subsamples a [`Task`] to use only a fraction of the rows.
+#' Subsamples a [`Task`] to use a fraction of the rows. `frac`
+#' can be > 1 if `replace` is `TRUE`.
 #'
 #' @section Parameter Set:
 #' * `frac` :: `numeric(1)` \cr
 #'   Fraction of rows in the task to keep. Default 1.
 #' * `stratify` :: `logical(1)` \cr
 #'   Should the subsamples be stratified by target? Default `FALSE`.
+#' * `replace` :: `logical(1)` \cr
+#'   Sample with replacement? Default is `FALSE`.
 #'
 #' @family PipeOps
 #' @include PipeOpTaskPreproc.R
 #' @export
-PipeOpDownsample = R6Class("PipeOpDownsample",
+PipeOpSample = R6Class("PipeOpSubsample",
   inherit = PipeOpTaskPreproc,
 
 #FIXME: remove stratify for now? does not work for regression.
 # or robustify it
 
   public = list(
-    initialize = function(id = "downsample") {
+    initialize = function(id = "subsample") {
       ps = ParamSet$new(params = list(
         ParamDbl$new("frac", default = 1, lower = 0, upper = 1),
-        ParamLgl$new("stratify", default = FALSE)
+        ParamLgl$new("stratify", default = FALSE),
+        ParamLgl$new("replace", default = FALSE)
       ))
       super$initialize(id, param_set = ps, can_subset_cols = FALSE)
-      self$param_vals = list(frac = 1, stratify = FALSE)
+      self$param_vals = list(frac = 1, stratify = FALSE, replace = FALSE)
     },
 
     train_task = function(task) {
       if (!self$param_vals$stratify) {
-        keep = sample(task$row_roles$use, ceiling(self$param_vals$frac * task$nrow))
+        keep = sample(task$row_roles$use, ceiling(self$param_vals$frac * task$nrow), replace = self$param_vals$replace)
       } else {
         if (!inherits(task, "TaskClassif"))
           stopf("Stratification not supported for %s", class(task))
         splt = split(task$row_roles$use, task$data(cols = task$target_names))
-        keep = unlist(map(splt, function(x) sample(x, ceiling(self$param_vals$frac * length(x)))))
+        keep = unlist(map(splt, function(x) sample(x, ceiling(self$param_vals$frac * length(x)), replace = self$param_vals$replace)))
       }
       self$state = list()
       task$filter(keep)
@@ -49,4 +53,4 @@ PipeOpDownsample = R6Class("PipeOpDownsample",
 )
 
 #' @include mlr_pipeops.R
-mlr_pipeops$add("downsample", PipeOpDownsample)
+mlr_pipeops$add("subsample", PipeOpSubsample)
