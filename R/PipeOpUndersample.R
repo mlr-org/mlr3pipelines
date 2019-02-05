4cf71593deabf6779cdeac4a94d9bbf8795d4508
mb706 <mlr.developer@mb706.com>
2019-02-05 18:30:56 +0100

    PipeOpUndersample complete

--- R/PipeOpUndersample.R
+++ R/PipeOpUndersample.R
@@ -6,9 +6,16 @@
 #' @description
 #' Undersamples (filters) a [`Task`] to keep only a fraction of the rows with majority class.
 #'
+#' Possible parameters are `$frac` and `$ratio`, only one of which must be given. The majority
+#' class is never "up"-sampled, so if `$ratio` times the mean minority class size turns out
+#' larger than the initial majority class size, the number of rows is shuffled but not changed.
+#'
 #' @section Parameter Set:
-#' * `frac` :: `numeric(1)` \cr
-#'   Fraction of rows of the majority class to keep in the task. Default 1.
+#' * `frac`  :: `numeric(1)` \cr
+#'   Fraction of rows of the majority class to keep in the task.
+#' * `ratio` :: `numeric(1)` \cr
+#'   Ratio of number of rows of majority class to keep, relative
+#'   to average minority class row number. Default is 1.
 #'
 #' @family PipeOps
 #' @include PipeOpTaskPreproc.R
@@ -19,20 +26,35 @@ PipeOpUndersample = R6Class("PipeOpUndersample",
   public = list(
     initialize = function(id = "undersample") {
       ps = ParamSet$new(params = list(
-        ParamDbl$new("frac", default = 1, lower = 0, upper = 1)
+        ParamDbl$new("frac", lower = 0, upper = 1, special_vals = list(NULL)),
+        ParamDbl$new("ratio", lower = 0, upper = Inf, special_vals = list(NULL), default = 1)
       ))
       super$initialize(id, param_set = ps)
-      self$param_vals = list(frac = 1)
+      self$param_vals = list(ratio = 1)
     },
 
     train_task = function(task) {
-      row_ids = task$row_ids
-      tab = data.table(row_ids = row_ids, label = task$truth(row_ids))[, list(N = .N, ids = list(row_ids)), by = "label"]
-      majority = tab[which_max(get("N"))]$label
-      tab[get("label") == majority, ids := list(list(sample(ids[[1L]], length(ids[[1L]]) * frac)))]
       self$state = list()
-      task$filter(unlist(tab$ids, use.names = FALSE))
-    }
+      classtbl = sort(table(task$truth()), decreasing = TRUE)
+      frac = self$param_vals$frac
+      ratio = self$param_vals$ratio
+      if (!is.null(frac)) {
+        if (!is.null(ratio)) {
+          stop("Only one of 'ratio' and 'frac' params must be given.")
+        }
+        keep_major = seq_len(classtbl[1]) <= round(frac * classtbl[1])
+      } else {
+        if (is.null(ratio)) {
+          stop("One of 'ratio' or 'frac' params must be given.")
+        }
+        keep_major = seq_len(classtbl[1]) <= round(ratio * mean(classtbl[-1]))
+      }
+      keep_all = rep(TRUE, task$nrow)
+      keep_all[task$truth() == names(classtbl)[1]] = sample(keep_major)
+      task$filter(which(keep_all))
+    },
+
+    predict_task = identity
   )
 )
 
