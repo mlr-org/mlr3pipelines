011ff2d3551ff7ea268fc679bcb8df2a1cb3ea56
mb706 <mlr.developer@mb706.com>
2019-02-10 17:34:47 +0100

    another paradox workaround

--- R/GraphLearner.R
+++ R/GraphLearner.R
@@ -23,11 +23,11 @@ GraphLearner = R6Class("GraphLearner", inherit = Learner,
       param_vals = insert_named(self$graph$param_set$values, param_vals)
       super$initialize(id = id, task_type = task_type,
         feature_types = mlr_reflections$task_feature_types,
-        predict_type = predict_type,
         predict_types = mlr_reflections$predict_types[[task_type]],
         packages = graph$packages,
+        param_set = ParamSet$new(list(ParamDbl$new("DUMMY"))),  # FIXME: workaround for mlr-org/paradox#219
         properties = mlr_reflections$learner_properties[[task_type]])
-      private$.predict_type = "response"
+      private$.predict_type = predict_type
       self$graph$param_set$values = param_vals
     },
     train = function(task) {
