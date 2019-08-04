1173ae3160c89f7e0829359fb9e082ac7bdeeb80
mb706 <mlr.developer@mb706.com>
2019-08-04 18:00:58 +0200

    small doc fixes

--- R/PipeOpBranch.R
+++ R/PipeOpBranch.R
@@ -90,17 +90,17 @@ mlr_pipeops$add("branch", PipeOpBranch, list("N"))
 #' @description
 #' Create a multiplexed graph.
 #'
-#' @param `...`:
+#' @param ...
 #'   Multiple graphs, possibly named. They all must have exactly
 #'   one output. If any of the arguments are named, then all must have
 #'   unique names.
-#' @param `.graphs` ([list of Graph]):
+#' @param .graphs (`[list of Graph]`):
 #'   Named list of Graphs, additionally to the graphs given in `...`.
-#' @param `.prefix_branchops` (`[character(1)]`):
+#' @param .prefix_branchops (`[character(1)]`):
 #'   Optional id prefix to prepend to [`PipeOpBranch`] and [`PipeOpUnbranch`] id. Their
 #'   resulting IDs will be `"[.prefix_branchops]branch"` and `"[.prefix_branchops]unbranch"`.
 #'   Default is `""`.
-#' @param `.prefix_names` (`[logical(1) | character(1)]`):
+#' @param .prefix_names (`[logical(1) | character(1)]`):
 #'   Whether to add prefixes to graph IDs when performing gunion. Can be helpful to
 #'   avoid ID clashes in resulting graph. Default `FALSE`. If this is `TRUE`, the prefixes
 #'   are taken from the names of the input arguments if present or `"poX"` where X counts up. If this is
@@ -113,12 +113,12 @@ mlr_pipeops$add("branch", PipeOpBranch, list("N"))
 #' branch(pca = po_pca, nothing = po_nop)
 #' # gives the same as
 #' branches = c("pca", "nothing")
-#' PipeOpBranch$new(branches) %>>% gunion(po_pca, po_nop) %>>% PipeOpUnbranch$new(branches)
+#' PipeOpBranch$new(branches) %>>% gunion(list(po_pca, po_nop)) %>>% PipeOpUnbranch$new(branches)
 #'
 #' branch(pca = po_pca, nothing = po_nop, .prefix_branchops = "br_", .prefix.paths = "xy_")
 #' #gives the same as
 #' PipeOpBranch$new(branches, id = "br_branch") %>>%
-#'   gunion(xy_pca = po_pca, xy_nothing = po_nop) %>>%
+#'   gunion(list(xy_pca = po_pca, xy_nothing = po_nop)) %>>%
 #'   PipeOpUnbranch$new(branches, id = "br_unbranch")
 #'
 #' @export
