8f1715b4547c4e39d8ff966663a81be1da356a0f
susanne-207 <dandl.susanne@gmail.com>
2019-08-16 10:35:45 +0200

    Modified R6 class PipeOpModelMatrix and added tests for class

--- R/PipeOpModelMatrix.R
+++ R/PipeOpModelMatrix.R
@@ -7,9 +7,6 @@
 #' Transforms columns using a given `formula`.
 #' It therefore uses the `stats::model.matrix()` function.
 #'
-#' Use the `$affect_columns` functionality to only encode a subset of columns,
-#' or only encode columns of a certain type.
-#'
 #' @section Parameter Set:
 #' * `formula`  :: `formula` \cr Formula to use. Higher order interactions
 #' can be created using constructs like `~. ^ 2`.
@@ -22,56 +19,14 @@ PipeOpModelMatrix = R6Class("PipeOpModelMatrix",
     initialize = function(id = "modelmatrix", param_vals = list()) {
       ps = ParamSet$new(params = list(
         ParamUty$new("formula")
-        # ParamFct$new("affect.type", levels = c("numeric",
-        #   "factor", "ordered", "other", NULL), default = NULL),
-        # ParamInt$new("affect.index", default = integer(0)),
-        # ParamFct$new("affect.names", default = character(0)),
-        # ParamFct$new("affect.pattern", default = NULL),
-        # ParamLgl$new("affect.invert", default = TRUE),
-        # ParamLgl$new("affect.pattern.ignore.case", default = FALSE),
-        # ParamLgl$new("affect.pattern.perl", default = FALSE),
-        # ParamLgl$new("affect.pattern.fixed", default = FALSE)
       ))
-      super$initialize(id, param_set = ps, param_vals = param_vals, packages = "stats")
+      super$initialize(id, param_set = ps, param_vals = param_vals,
+        packages = "stats")
+    },
+
+    transform_dt = function(dt, levels) {
+      as.data.frame(model.matrix(self$param_set$values$formula, data = dt))
     }
-
-    # select_cols = function(task) {
-    #   task$feature_types[get("type") %in%  c("numeric",
-    #        "factor", "ordered"), get("id")]
-    # }
-
-    # get_state_dt = function(dt, levels) {
-    #   contrasts = switch(self$param_set$values$method,
-    #     "one-hot" = function(x) stats::contr.treatment(x, contrasts = FALSE),
-    #     treatment = stats::contr.treatment,
-    #     helmert = stats::contr.helmert,
-    #     poly = function(x) {
-    #       cont = stats::contr.poly(x)
-    #       rownames(cont) = x
-    #       colnames(cont) = NULL
-    #       cont
-    #     },
-    #     sum = stats::contr.sum,
-    #     stop("Unknown 'method' parameter value.")
-    #   )
-    #   list(contrasts = lapply(levels, function(x) {
-    #     con = contrasts(x)
-    #     if (is.null(colnames(con))) {
-    #       colnames(con) = as.character(seq_len(ncol(con)))
-    #     }
-    #     con
-    #   }))
-    # },
-    #
-    # transform_dt = function(dt, levels) {
-    #   cols = imap(self$state$contrasts, function(contrasts, id) {
-    #     x = dt[[id]]
-    #     contrasts[as.character(x), , drop = FALSE]
-    #   })
-    #   cols = as.data.table(cols)
-    #   setnames(cols, names(cols), make.names(names(cols), unique = TRUE))
-    #   cols
-    # }
   )
 )
 
---
+++ tests/testthat/test_pipeop_modelmatrix.R
@@ -0,0 +1,52 @@
+context("PipeOpModelMatrix")
+
+test_that("PipeOpModelMatrix - basic properties", {
+
+  task = mlr_tasks$get("iris")
+  # General
+  expect_datapreproc_pipeop_class(PipeOpModelMatrix,
+    constargs = list(param_vals = list(formula = ~ . ^ 2)), task = task)
+
+  # Intercept
+  op = PipeOpModelMatrix$new(param_vals = list(formula = ~ . ^ 2))
+  expect_pipeop(op)
+  nt = train_pipeop(op, inputs = list(task))[[1L]]
+  fn = nt$feature_names
+  expect_true("(Intercept)" %in% fn) # Intercept
+  expect_equal(length(grep(":", fn)), 6) # 6 interaction terms
+  expect_equal(length(fn), 6 + 4 + 1) # 6 interaction terms + 4 main + intercept
+
+  multiplyCols <- function(df, nams){
+    name1 = nams[[1]]
+    name2 = nams[2]
+    df[, name1, with = FALSE] * df[, name2, with = FALSE]
+  }
+
+  fn.inter = fn[grep(":", fn)]
+  fn.sing = strsplit(fn.inter, ":")
+  nt.dat = nt$data()
+  sapply(1:3, function(i) {
+    expect_true(all(nt.dat[, get(fn.inter[i])] ==
+        multiplyCols(nt.dat, fn.sing[[i]])))
+  })
+
+  # Without intercept
+  op = PipeOpModelMatrix$new(param_vals = list(formula = ~ 0 + Sepal.Length))
+  expect_pipeop(op)
+  nt = train_pipeop(op, inputs = list(task))[[1L]]
+  fn = nt$feature_names
+  expect_true("(Intercept)" %nin% fn)
+  expect_true("Sepal.Length" %in% fn)
+
+  # other formula
+  op = PipeOpModelMatrix$new(param_vals = list(formula = ~ 0 + Sepal.Length +
+      log(Sepal.Length)))
+  expect_pipeop(op)
+  nt = train_pipeop(op, inputs = list(task))[[1L]]
+  fn = nt$feature_names
+  nt.dat = nt$data()
+  expect_true(all(nt.dat[, "log(Sepal.Length)", with = TRUE] ==
+      log(nt.dat[, Sepal.Length])))
+
+})
+
