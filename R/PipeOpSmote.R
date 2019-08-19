d301914ec3fc1d679745dd9e4f3947c4fdbd63f7
susanne-207 <dandl.susanne@gmail.com>
2019-08-19 18:43:58 +0200

    Replaces DMwR::SMOTE by smotefamily::SMOTE + added docu

--- R/PipeOpSmote.R
+++ R/PipeOpSmote.R
@@ -2,11 +2,15 @@
 #'
 #' @usage NULL
 #' @name mlr_pipeops_smote
-#' @format [`R6Class`] object inheriting from [`PipeOpTaskPreprocSimple`]/[`PipeOpTaskPreproc`]/[`PipeOp`].
+#' @format [`R6Class`] object inheriting from [`PipeOpTaskPreproc`]/[`PipeOp`].
 #'
 #' @description
-#' Adds features according to expressions given as formulas that may depend on values of other features.
-#' This can add new features, or can change existing features.
+#' Generates a more balanced data set by creating
+#' synthetic instances of the minority class using the SMOTE algorithm.
+#' The algorithm samples for each minority instance a new data point based on the `K` nearest
+#' neighbors of that data point.
+#' It can only be applied to tasks with numeric features.
+#' See [`smotefamily::SMOTE`] for details.
 #'
 #' @section Construction:
 #' ```
@@ -20,42 +24,46 @@
 #' @section Input and Output Channels:
 #' Input and output channels are inherited from [`PipeOpTaskPreproc`].
 #'
-#' The output is the input [`Task`][mlr3::Task] with added and/or smoted features according to the `mutation` parameter.
+#' The output is the input [`Task`][mlr3::Task] with added synthetic rows for the minority class.
 #'
 #' @section State:
 #' The `$state` is a named `list` with the `$state` elements inherited from [`PipeOpTaskPreproc`].
 #'
 #' @section Parameters:
 #' The parameters are the parameters inherited from [`PipeOpTaskPreproc`], as well as:
-#' * `mutation` :: named `list` of `formula`\cr
-#'   Expressions for new features to create (or present features to change), in the form of `formula`.
-#'   Each element of the list is a `formula` with the name of the element naming the feature to create or
-#'   change, and the formula expression determining the result. This expression may reference
-#'   other features, as well as variables visible at the creation of the `formula` (see examples).
-#'   Initialized to `list()`.
-#' * `delete_originals` :: `logical(1)` \cr
-#'   Whether to delete original features. Even when this is `FALSE`,
-#'   present features may still be overwritten. Initialized to `FALSE`.
+#' * `K` :: `numeric(1)` \cr
+#'   The number of nearest neighbors used for sampling new values.
+#'   See [`SMOTE`][`smotefamily::SMOTE`].
+#' * `dup_size` :: `numeric` \cr
+#'   Desired times of synthetic minority instances over the original number of
+#'   majority instances. See [`SMOTE`][`smotefamily::SMOTE`].
 #'
 #' @section Internals:
-#' A `formula` created using the `~` operator always contains a reference to the `environment` in which
-#' the `formula` is created. This makes it possible to use variables in the `~`-expressions that both
-#' reference either column names or variable names.
+#' For details see: \cr
+#' Chawla, N., Bowyer, K., Hall, L. and Kegelmeyer, W. 2002.
+#' SMOTE: Synthetic minority oversampling technique.
+#' Journal of Artificial Intelligence Research. 16, 321-357.
 #'
 #' @section Fields:
 #' Only fields inherited from [`PipeOpTaskPreproc`]/[`PipeOp`].
 #'
 #' @section Methods:
-#' Only methods inherited from [`PipeOpTaskPreprocSimple`]/[`PipeOpTaskPreproc`]/[`PipeOp`].
+#' Only methods inherited from [`PipeOpTaskPreproc`]/[`PipeOp`].
 #'
 #' @examples
-#' pom = mlr_pipeops$get("smote")
-#' pom$param_set$values$mutation = list(
-#'   Sepal.Area = ~ Sepal.Width * Sepal.Length,
-#'   Petal.Area = ~ Petal.Width * Petal.Length
-#' )
+#' pop = mlr_pipeops$get("smote")
 #'
-#' pom$train(list("iris"))[[1]]$data()
+#' # Create example task
+#' library(smotefamily)
+#' data_example = sample_generator(1000,ratio = 0.80)
+#' task = TaskClassif$new(id = "example", backend = data_example, target = "result")
+#' task$data()
+#' table(task$data()$result)
+#'
+#' # Generate synthetic data for minority class
+#' smotedata = pop$train(list(task))[[1]]$data()
+#' table(smotedata$result)
+#'
 #' @family PipeOps
 #' @include PipeOpTaskPreproc.R
 #' @export
@@ -64,22 +72,33 @@ PipeOpSmote = R6Class("PipeOpSmote",
   public = list(
     initialize = function(id = "smote", param_vals = list()) {
       ps = ParamSet$new(params = list(
-        ParamUty$new("form", tags = "required"),
-        ParamInt$new("perc.over", default = 200),
-        ParamInt$new("k", default = 5),
-        ParamInt$new("perc.under", default = 200),
-        ParamUty$new("learner", default = NULL)
+        ParamInt$new("K", default = 5),
+        ParamInt$new("dup_size", default = 0)
       ))
-      super$initialize(id, param_set = ps, param_vals = param_vals, packages = "DMwR")
+      super$initialize(id, param_set = ps, param_vals = param_vals,
+        packages = "smotefamily")
+    },
+
+    select_cols = function(task) {
+      task$feature_types[get("type") %in% c("numeric"), get("id")]
     },
 
     train_task = function(task) {
+      assert_true(all(task$feature_types$type == "numeric"))
+      dt = task$data(cols = task$feature_names)
+
+      # extract info to use smotefamily::SMOTE
       ps = self$param_set$values
-      dt_old = task$data()
-      ps$data = dt_old
-      dt = as.data.table(invoke(DMwR::SMOTE, .args = ps))
-      ###### TO DO: Add new data to task########
-      return(task)
+      ps$X = as.data.frame(dt)
+      ps$target = c(task$data(cols = task$target_names))[[1]]
+
+      # calculate synthetic data
+      st = invoke(smotefamily::SMOTE, .args = ps)$syn_data
+
+      # add synthetic data to task data
+      target.id = which(names(st) == "class")
+      colnames(st)[target.id] = task$target_names
+      task$rbind(st)
     }
   )
 )
