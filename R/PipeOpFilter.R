12bab987d2ec4676473487c4a62c6df401bbddd9
mb706 <mlr.developer@mb706.com>
2019-08-19 13:19:50 +0200

    PipeOpFilter paramset (closes #203)

--- R/PipeOpFilter.R
+++ R/PipeOpFilter.R
@@ -92,12 +92,11 @@ PipeOpFilter = R6Class("PipeOpFilter",
       assert_class(filter, "Filter")
       self$filter = filter$clone(deep = TRUE)
       self$filter$param_set$set_id = ""
-      ps = ParamSet$new(list(
+      private$.outer_param_set = ParamSet$new(list(
         ParamInt$new("nfeat", lower = 0),
         ParamDbl$new("frac", lower = 0, upper = 1),
         ParamDbl$new("cutoff")
       ))
-      private$.outer_param_set = ps$add(filter$param_set)
       private$.outer_param_set$set_id = "filter"
       super$initialize(id, self$param_set, param_vals = param_vals)
     },
