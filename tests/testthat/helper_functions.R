749f5c0106a4730de298aedb7953ca4f3cdb5e3a
Michel Lang <michellang@gmail.com>
2019-12-06 19:07:44 +0100

    uncomment skip_on_cran again

--- tests/testthat/helper_functions.R
+++ tests/testthat/helper_functions.R
@@ -113,7 +113,7 @@ expect_pipeop = function(po) {
 # - *_internal checks for classes
 # - *_internal handles NO_OP as it should
 expect_pipeop_class = function(poclass, constargs = list()) {
-  # skip_on_cran()
+  skip_on_cran()
   po = do.call(poclass$new, constargs)
 
   expect_pipeop(po)
