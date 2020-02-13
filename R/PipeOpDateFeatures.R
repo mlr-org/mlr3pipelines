37bc438d4af74839585cc91b49d0503bb86f4000
sumny <lennart.sch@web.de>
2020-02-13 22:00:22 +0100

    allow dispatch on multiple columns; use transform_dt; minor cosmetic code changes

--- R/PipeOpDateFeatures.R
+++ R/PipeOpDateFeatures.R
@@ -1,15 +1,15 @@
 #' @title PipeOpDateFeatures
 #'
 #' @usage NULL
-#' @name mlr_pipeops_datefeaturs
-#' @format [`R6Class`] object inheriting from [`PipeOpTaskPreprocSimple`]/[`PipeOp`].
+#' @name mlr_pipeops_datefeatures
+#' @format [`R6Class`] object inheriting from [`PipeOpTaskPreprocSimple`]/[`PipeOpTaskPreproc`]/[`PipeOp`].
 #'
 #' @description
-#' Based on a specified `POSIXct` column of the data, a set of date related features is computed and
-#' added to the feature set of the output task. If no `POSIXct` column is specified, the first
-#' `POSIXct` column found in the data is used. If none is found, the original task is returned
+#' Based on `POSIXct` columns of the data, a set of date related features is computed and added to
+#' the feature set of the output task. If no `POSIXct` column is found, the original task is returned
 #' unaltered. This functionality is based on the `add_datepart()` and `add_cyclic_datepart()`
-#' functions from the `fastai` library.
+#' functions from the `fastai` library. If operation on only particular `POSIXct` columns is
+#' requested, use the `affect_columns` parameter inherited from [`PipeOpTaskPreprocSimple`].
 #'
 #' @section Construction:
 #' ```
@@ -25,47 +25,39 @@
 #' Input and output channels are inherited from [`PipeOpTaskPreprocSimple`].
 #'
 #' The output is the input [`Task`][mlr3::Task] with date-related features computed and added to the
-#' feature set of the output task and the specified `POSIXct` column of the data removed from the
+#' feature set of the output task and the `POSIXct` columns of the data removed from the
 #' feature set (depending on the value of `keep_date_var`).
 #'
 #' @section State:
 #' The `$state` is a named `list` with the `$state` elements inherited from
-#' [`PipeOpTaskPreprocSimple`], as well as:
-#' * `date_var` the name of the `POSIXct` column that was used for the feature engineering.
-#' * `keep_date_var` logical value indicating whether the `date_var` column of the data should be
-#'   removed from the feature set.
-#' * `cyclic` logical value indicating whether cyclic features should be computed.
-#' * `features` vector of computed feature names.
+#' [`PipeOpTaskPreprocSimple`].
 #'
 #' @section Parameters:
 #' The parameters are the parameters inherited from [`PipeOpTaskPreprocSimple`], as well as:
-#' * `date_var` :: `character(1)`\cr
-#'   The name of the `POSIXct` column of the data that should be used for the feature engineering.
-#'   If not specified, the first `POSIXct` column found in the data will be used.
 #' * `keep_date_var` :: `logical(1)`\cr
-#'   Whether the `date_var` column should be kept as a feature. Default FALSE.
+#'   Should the `POSIXct` columns be kept as features? Default FALSE.
 #' * `cyclic` :: `logical(1)`\cr
-#'   Whether cyclic features should be computed. See Internals. Default FALSE.
+#'   Should cyclic features be computed? See Internals? Default FALSE.
 #' * `year` :: `logical(1)`\cr
-#'   Whether the year should be extracted as a feature. Default TRUE.
+#'   Should the year be extracted as a feature? Default TRUE.
 #' * `month` :: `logical(1)`\cr
-#'   Whether the month should be extracted as a feature. Default TRUE.
+#'   Should the month be extracted as a feature? Default TRUE.
 #' * `week_of_year` :: `logical(1)`\cr
-#'   Whether the week of the year should be extracted as a feature. Default TRUE.
+#'   Should the week of the year be extracted as a feature? Default TRUE.
 #' * `day_of_year` :: `logical(1)`\cr
-#'   Whether the day of the year should be extracted as a feature. Default TRUE.
+#'   Should the day of the year be extracted as a feature? Default TRUE.
 #' * `day_of_month` :: `logical(1)`\cr
-#'   Whether the day of the month should be extracted as a feature. Default TRUE.
+#'   Should the day of the month be extracted as a feature? Default TRUE.
 #' * `day_of_week` :: `logical(1)`\cr
-#'   Whether the day of the week should be extracted as a feature. Default TRUE.
+#'   Should the day of the week be extracted as a feature? Default TRUE.
 #' * `hour` :: `logical(1)`\cr
-#'   Whether the hour should be extracted as a feature. Default TRUE.
+#'   Should the hour be extracted as a feature? Default TRUE.
 #' * `minute` :: `logical(1)`\cr
-#'   Whether the minute should be extracted as a feature. Default TRUE.
+#'   Should the minute be extracted as a feature? Default TRUE.
 #' * `second` :: `logical(1)`\cr
-#'   Whether the second should be extracted as a feature. Default TRUE.
+#'   Should the second be extracted as a feature? Default TRUE.
 #' * `is_day` :: `logical(1)`\cr
-#'   Whether a feature should be extracted indicating whether it is day time (06:00am - 08:00pm).
+#'   Should a feature be extracted indicating whether it is day time (06:00am - 08:00pm)?
 #'   Default TRUE.
 #'
 #' @section Internals:
@@ -87,8 +79,7 @@
 #' dat$date = sample(seq(as.POSIXct("2020-02-01"), to = as.POSIXct("2020-02-29"), by = "hour"),
 #'   size = 150L)
 #' task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-#' pop = po("datefeatures", param_vals = list(date_var = "date", cyclic = FALSE, minute = FALSE,
-#'   second = FALSE))
+#' pop = po("datefeatures", param_vals = list(cyclic = FALSE, minute = FALSE, second = FALSE))
 #' pop$train(list(task))
 #' pop$state
 #' @family PipeOps
@@ -99,75 +90,69 @@ PipeOpDateFeatures = R6Class("PipeOpDateFeatures",
   public = list(
     initialize = function(id = "datefeatures", param_vals = list()) {
       ps = ParamSet$new(params = list(
-        ParamUty$new("date_var", custom_check = check_character, tags = "train"),
-        ParamLgl$new("keep_date_var", tags = c("train", "required")),
-        ParamLgl$new("cyclic", tags = c("train", "required")),
-        ParamLgl$new("year", tags = c("train", "required")),
-        ParamLgl$new("month", tags = c("train", "required")),
-        ParamLgl$new("week_of_year", tags = c("train", "required")),
-        ParamLgl$new("day_of_year", tags = c("train", "required")),
-        ParamLgl$new("day_of_month", tags = c("train", "required")),
-        ParamLgl$new("day_of_week", tags = c("train", "required")),
-        ParamLgl$new("hour", tags = c("train", "required")),
-        ParamLgl$new("minute", tags = c("train", "required")),
-        ParamLgl$new("second", tags = c("train", "required")),
-        ParamLgl$new("is_day", tags = c("train", "required"))
+        ParamLgl$new("keep_date_var", tags = c("train", "predict", "required")),
+        ParamLgl$new("cyclic", tags = c("train", "predict", "required")),
+        ParamLgl$new("year", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("month", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("week_of_year", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("day_of_year", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("day_of_month", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("day_of_week", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("hour", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("minute", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("second", tags = c("train", "predict", "datepart", "required")),
+        ParamLgl$new("is_day", tags = c("train", "predict", "datepart", "required"))
       ))
-      ps$values = list(date_var = character(0L), keep_date_var = FALSE, cyclic = FALSE, year = TRUE,
+      ps$values = list(keep_date_var = FALSE, cyclic = FALSE, year = TRUE,
         month = TRUE, week_of_year = TRUE, day_of_year = TRUE, day_of_month = TRUE,
         day_of_week = TRUE, hour = TRUE, minute = TRUE, second = TRUE, is_day = TRUE)
       super$initialize(id = id, param_set = ps, param_vals = param_vals)
     },
 
-    get_state = function(task) {
-      date_var = self$param_set$values$date_var
-      # if no POSIXct column was specified use the first POSIXct column found
-      if (length(date_var) == 0L) {
-        date_var = task$feature_names[task$feature_types$type == "POSIXct"][1L]
-        # this is NA if no POSIXct column was found
-      }
-      list(
-        date_var = date_var,
-        keep_date_var = self$param_set$values$keep_date_var,
-        cyclic = self$param_set$values$cyclic,
-        features = setdiff(self$param_set$ids("ParamLgl")[unlist(self$param_set$get_values("ParamLgl"))],
-          c("keep_date_var", "cyclic"))
-      )
+    select_cols = function(task) {
+      task$feature_types[get("type") == "POSIXct", get("id")]
     },
 
-    transform = function(task) {
-      # early exit if no POSIXct column was specified or found, or no features are to be computed
-      # return the unaltered task
-      if (is.na(self$state$date_var) || (length(self$state$features) == 0L)) {
-        warning("No `POSIXct` column found or no features to compute. ",
-          "Returning the task unaltered.")
-        return(task)
+    transform_dt = function(dt, levels) {
+      features = names(which(unlist(self$param_set$get_values(tags = "datepart"))))
+      # early exit if no features are to be computed
+      if (length(features) == 0L) {
+        return(dt)
       }
-      dates = task$data(cols = self$state$date_var)
-      assert_posixct(dates[[1L]])
+
       # special handling of year because this is needed for day_of_year and day_of_month
-      features = unique(c("year", self$state$features))
+      drop_year = "year" %nin% features
+      features = unique(c("year", features))
 
-      dates[, (features) := compute_date_features(get(self$state$date_var), features = features)]
+      cyclic_features = features[features %in% c("month", "week_of_year", "day_of_year",
+        "day_of_month", "day_of_week", "hour", "minute", "second")]
+
+      date_vars = colnames(copy(dt))
+
+      for (date_var in date_vars) {
+        dt[, paste0(date_var, ".", features) :=
+          compute_date_features(get(date_var), features = features)]
+      }
+
       # if cyclic = TRUE for month, week_of_year, day_of_year, day_of_month, day_of_week, hour,
-      # minute and second two columns are additionally added, each consisting of their sinus and
+      # minute and second, two columns are additionally added, each consisting of their sinus and
       # cosinus transformation of in general 2 * pi * x / max_x (x starting from 0)
-      if (self$state$cyclic) {
-        cyclic_features = features[features %in%
-          c("month", "week_of_year", "day_of_year", "day_of_month", "day_of_week",
-            "hour", "minute", "second")]
-        dates[, paste0(rep(cyclic_features, each = 2L), "_", c("sin", "cos")) :=
-          compute_cyclic_date_features(.SD, features = cyclic_features)]
-        #dates[, (cyclic_features) := NULL] # drop the original features
+      if (self$param_set$values$cyclic && (length(cyclic_features) > 0L)) {
+        for (date_var in date_vars) {
+          dt[, paste0(date_var, ".", rep(cyclic_features, each = 2L), "_", c("sin", "cos")) :=
+            compute_cyclic_date_features(.SD, features = cyclic_features, date_var = date_var)]
+        }
       }
 
-      task$cbind(dates)
-      if (self$state$keep_date_var) {
-        task$select(setdiff(task$feature_names, setdiff(features, self$state$features)))
-      } else {
-        task$select(setdiff(task$feature_names,
-           c(setdiff(features, self$state$features), self$state$date_var)))
+      if (self$param_set$values$keep_date_var == FALSE) {
+        dt[, (date_vars) := NULL]
       }
+
+      if (drop_year) {
+        dt[, paste0(date_vars, ".", "year") := NULL]
+      }
+
+      dt
     }
   )
 )
@@ -198,21 +183,22 @@ compute_date_features = function(dates, features) {
 
 # helper function to compute cyclic date features of date features, i.e.,
 # sinus and cosinus transformations of 2 * pi * x / max_x
-compute_cyclic_date_features = function(date_features, features) {
+compute_cyclic_date_features = function(date_features, features, date_var) {
   do.call(c, args = lapply(features, FUN = function(feature) {
     # all values are expected to start at 0 and therefore may be shifted by - 1
     value = if (feature %in% c("month", "week_of_year", "day_of_year", "day_of_month")) {
-      date_features[[feature]] - 1L
+      date_features[[paste0(date_var, ".", feature)]] - 1L
     } else {
-      date_features[[feature]]
+      date_features[[paste0(date_var, ".", feature)]]
     }
     maximum = switch(feature,
       month = 12L,
       week_of_year = 52L,
-      day_of_year = (365L + as.integer((date_features[["year"]] %% 400L == 0L) |
-        (date_features[["year"]] %% 4L == 0L & date_features[["year"]] %% 100L != 0L))),
-      day_of_month = get_days_per_month(date_features[["year"]],
-        month = date_features[["month"]]),
+      day_of_year = (365L + as.integer((date_features[[paste0(date_var, ".", "year")]] %% 400L == 0L) |
+        (date_features[[paste0(date_var, ".", "year")]] %% 4L == 0L &
+        date_features[[paste0(date_var, ".", "year")]] %% 100L != 0L))),
+      day_of_month = get_days_per_month(date_features[[paste0(date_var, ".", "year")]],
+        month = date_features[[paste0(date_var, ".", "month")]]),
       day_of_week = 7L,
       hour = 24L,
       minute = 60L,
--- tests/testthat/test_pipeop_datefeatures.R
+++ tests/testthat/test_pipeop_datefeatures.R
@@ -6,11 +6,11 @@ test_that("PipeOpDateFeatures - basic properties", {
   dat$date = sample(seq(as.POSIXct("2020-01-31"), to = as.POSIXct("2020-03-01"), by = "sec"),
     size = 150L)
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date"))
+  po = PipeOpDateFeatures$new()
   expect_pipeop(po)
   train_pipeop(po, inputs = list(task))
   predict_pipeop(po, inputs = list(task))
-  suppressWarnings(expect_datapreproc_pipeop_class(PipeOpDateFeatures, task = task)) # due to no POSIXct column
+  expect_datapreproc_pipeop_class(PipeOpDateFeatures, task = task)
 })
 
 test_that("PipeOpDateFeatures - finds POSIXct column", {
@@ -21,7 +21,6 @@ test_that("PipeOpDateFeatures - finds POSIXct column", {
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
   po = PipeOpDateFeatures$new()
   train_pipeop(po, inputs = list(task))
-  expect_true(po$state$date_var == "date")
 })
 
 test_that("PipeOpDateFeatures - unaltered if no POSIXct column", {
@@ -31,7 +30,7 @@ test_that("PipeOpDateFeatures - unaltered if no POSIXct column", {
     size = 150L)
   task = TaskClassif$new("iris_date", backend = iris, target = "Species")
   po = PipeOpDateFeatures$new()
-  expect_warning(train_pipeop(po, inputs = list(task)))
+  train_pipeop(po, inputs = list(task))
   expect_identical(po$state$intasklayout, po$state$outtasklayout)
 })
 
@@ -41,10 +40,10 @@ test_that("PipeOpDateFeatures - unaltered if no features specified", {
   dat$date = sample(seq(as.POSIXct("2020-01-31"), to = as.POSIXct("2020-03-01"), by = "sec"),
     size = 150L)
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date", cyclic = TRUE, year = FALSE,
+  po = PipeOpDateFeatures$new(param_vals = list(cyclic = TRUE, year = FALSE,
     month = FALSE, week_of_year = FALSE, day_of_year = FALSE, day_of_month = FALSE,
     day_of_week = FALSE, hour = FALSE, minute = FALSE, second = FALSE, is_day = FALSE))
-  expect_warning(train_pipeop(po, inputs = list(task)))
+  train_pipeop(po, inputs = list(task))
   expect_identical(po$state$intasklayout, po$state$outtasklayout)
 })
 
@@ -54,19 +53,19 @@ test_that("PipeOpDateFeatures - correct basic features", {
   dat$date = sample(seq(as.POSIXct("2020-01-31"), to = as.POSIXct("2020-03-01"), by = "sec"),
     size = 150L)
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date"))
+  po = PipeOpDateFeatures$new()
   trained_data = train_pipeop(po, inputs = list(task))$output$data()
-  expect_true(all(trained_data$year == as.numeric(format(dat$date, "%Y"))))
-  expect_true(all(trained_data$month == as.numeric(format(dat$date, "%m"))))
-  expect_true(all(trained_data$week_of_year == as.numeric(format(dat$date, "%U"))))
-  expect_true(all(trained_data$day_of_year == as.numeric(format(dat$date, "%j"))))
-  expect_true(all(trained_data$day_of_month == as.numeric(format(dat$date, "%d"))))
-  expect_true(all(trained_data$day_of_week == as.numeric(format(dat$date, "%w"))))
-  expect_true(all(trained_data$hour == as.numeric(format(dat$date, "%H"))))
-  expect_true(all(trained_data$minute == as.numeric(format(dat$date, "%M"))))
-  expect_true(all(trained_data$second == as.numeric(format(dat$date, "%S"))))
+  expect_true(all(trained_data$date.year == as.numeric(format(dat$date, "%Y"))))
+  expect_true(all(trained_data$date.month == as.numeric(format(dat$date, "%m"))))
+  expect_true(all(trained_data$date.week_of_year == as.numeric(format(dat$date, "%U"))))
+  expect_true(all(trained_data$date.day_of_year == as.numeric(format(dat$date, "%j"))))
+  expect_true(all(trained_data$date.day_of_month == as.numeric(format(dat$date, "%d"))))
+  expect_true(all(trained_data$date.day_of_week == as.numeric(format(dat$date, "%w"))))
+  expect_true(all(trained_data$date.hour == as.numeric(format(dat$date, "%H"))))
+  expect_true(all(trained_data$date.minute == as.numeric(format(dat$date, "%M"))))
+  expect_true(all(trained_data$date.second == as.numeric(format(dat$date, "%S"))))
   hours = as.numeric(format(dat$date, "%H"))
-  expect_true(all(trained_data$is_day == ((6 <= hours) & (hours <= 20))))
+  expect_true(all(trained_data$date.is_day == ((6 <= hours) & (hours <= 20))))
 })
 
 test_that("PipeOpDateFeatures - correct cyclic features", {
@@ -74,42 +73,41 @@ test_that("PipeOpDateFeatures - correct cyclic features", {
   set.seed(1)
   dat$date = sample(seq(as.POSIXct("2020-02-01"), to = as.POSIXct("2020-02-29"), by = "sec"),
     size = 150L)
-
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date", cyclic = TRUE))
+  po = PipeOpDateFeatures$new(param_vals = list(cyclic = TRUE))
   trained_data = train_pipeop(po, inputs = list(task))$output$data()
 
   month = as.numeric(format(dat$date, "%m")) - 1L
   value_scaled_month = 2L * pi * month / 12L
-  expect_identical(trained_data$month_sin, sin(value_scaled_month))
+  expect_identical(trained_data$date.month_sin, sin(value_scaled_month))
 
   week_of_year = as.numeric(format(dat$date, "%U")) - 1L
   value_scaled_woy = 2L * pi * week_of_year / 52L
-  expect_identical(trained_data$week_of_year_sin, sin(value_scaled_woy))
+  expect_identical(trained_data$date.week_of_year_sin, sin(value_scaled_woy))
 
   day_of_year = as.numeric(format(dat$date, "%j")) - 1L
   value_scaled_doy = 2L * pi * day_of_year / (365L + 1L)
-  expect_identical(trained_data$day_of_year_sin, sin(value_scaled_doy))
+  expect_identical(trained_data$date.day_of_year_sin, sin(value_scaled_doy))
 
   day_of_month = as.numeric(format(dat$date, "%d")) - 1L
   value_scaled_dom = 2L * pi * day_of_month / 29L
-  expect_identical(trained_data$day_of_month_sin, sin(value_scaled_dom))
+  expect_identical(trained_data$date.day_of_month_sin, sin(value_scaled_dom))
 
   day_of_week = as.numeric(format(dat$date, "%w"))
   value_scaled_dow = 2L * pi * day_of_week / 7L
-  expect_identical(trained_data$day_of_week_sin, sin(value_scaled_dow))
+  expect_identical(trained_data$date.day_of_week_sin, sin(value_scaled_dow))
 
   hour = as.numeric(format(dat$date, "%H"))
   value_scaled_hour = 2L * pi * hour / 24L
-  expect_identical(trained_data$hour_sin, sin(value_scaled_hour))
+  expect_identical(trained_data$date.hour_sin, sin(value_scaled_hour))
 
   minute = as.numeric(format(dat$date, "%M"))
   value_scaled_minute = 2L * pi * minute / 60L
-  expect_identical(trained_data$minute_sin, sin(value_scaled_minute))
+  expect_identical(trained_data$date.minute_sin, sin(value_scaled_minute))
 
   second = as.numeric(format(dat$date, "%S"))
   value_scaled_second = 2L * pi * second / 60L
-  expect_identical(trained_data$second_sin, sin(value_scaled_second))
+  expect_identical(trained_data$date.second_sin, sin(value_scaled_second))
 })
 
 test_that("PipeOpDateFeatures - feature selection works", {
@@ -118,16 +116,20 @@ test_that("PipeOpDateFeatures - feature selection works", {
   dat$date = sample(seq(as.POSIXct("2020-01-31"), to = as.POSIXct("2020-03-01"), by = "sec"),
     size = 150L)
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date", cyclic = TRUE, year = FALSE,
+  po = PipeOpDateFeatures$new(param_vals = list(cyclic = TRUE, year = FALSE,
     second = FALSE))
   expect_identical(train_pipeop(po, inputs = list(task))$output$feature_names,
     c("Petal.Length", "Petal.Width", "Sepal.Length", "Sepal.Width",
-      "month", "week_of_year", "day_of_year", "day_of_month", "day_of_week",
-      "hour", "minute", "is_day",
-      "month_sin", "month_cos", "week_of_year_sin", "week_of_year_cos",
-      "day_of_year_sin", "day_of_year_cos", "day_of_month_sin", "day_of_month_cos",
-      "day_of_week_sin", "day_of_week_cos",
-      "hour_sin", "hour_cos", "minute_sin", "minute_cos"))
+      paste0("date.",
+        c("month", "week_of_year", "day_of_year", "day_of_month", "day_of_week",
+          "hour", "minute", "is_day",
+          "month_sin", "month_cos", "week_of_year_sin", "week_of_year_cos",
+          "day_of_year_sin", "day_of_year_cos", "day_of_month_sin", "day_of_month_cos",
+          "day_of_week_sin", "day_of_week_cos",
+          "hour_sin", "hour_cos", "minute_sin", "minute_cos")
+      )
+    )
+  )
 })
 
 test_that("PipeOpDateFeatures - keep_date_var works", {
@@ -136,9 +138,8 @@ test_that("PipeOpDateFeatures - keep_date_var works", {
   dat$date = sample(seq(as.POSIXct("2020-01-31"), to = as.POSIXct("2020-03-01"), by = "sec"),
     size = 150L)
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date", keep_date_var = TRUE))
-  trained_task = train_pipeop(po, inputs = list(task))$output
-  expect_true("date" %in% trained_task$feature_names)
+  po = PipeOpDateFeatures$new(param_vals = list(keep_date_var = TRUE))
+  expect_true("date" %in% train_pipeop(po, inputs = list(task))$output$feature_names)
 })
 
 test_that("PipeOpDateFeatures - automatic NA handling", {
@@ -148,18 +149,18 @@ test_that("PipeOpDateFeatures - automatic NA handling", {
     size = 150L)
   dat$date[1L] = NA
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date", cyclic = TRUE))
+  po = PipeOpDateFeatures$new(param_vals = list(cyclic = TRUE))
   output = train_pipeop(po, inputs = list(task))$output
-  expect_true(all(is.na(output$data(rows = 1L, cols = output$feature_names[- (1:4)]))))
+  expect_true(all(is.na(output$data(rows = 1L, cols = output$feature_names[- (1L:4L)]))))
 })
 
 test_that("PipeOpDateFeatures - constant dates", {
   dat = iris
   dat$date = as.POSIXct("2020-01-31")
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date", cyclic = TRUE))
+  po = PipeOpDateFeatures$new(param_vals = list(cyclic = TRUE))
   output = train_pipeop(po, inputs = list(task))$output
-  expect_true(all(apply(output$data(cols = output$feature_names[- (1:4)]), 2, duplicated)[-1L, ]))
+  expect_true(all(apply(output$data(cols = output$feature_names[- (1L:4L)]), 2, duplicated)[-1L, ]))
 })
 
 test_that("PipeOpDateFeatures - no year but day_of_year and day_of_month", {
@@ -168,7 +169,50 @@ test_that("PipeOpDateFeatures - no year but day_of_year and day_of_month", {
   dat$date = sample(seq(as.POSIXct("2020-01-31"), to = as.POSIXct("2020-03-01"), by = "sec"),
     size = 150L)
   task = TaskClassif$new("iris_date", backend = dat, target = "Species")
-  po = PipeOpDateFeatures$new(param_vals = list(date_var = "date", cyclic = TRUE, year = FALSE))
-  output = train_pipeop(po, inputs = list(task))$output
-  expect_true("year" %nin% output$feature_names)
+  po = PipeOpDateFeatures$new(param_vals = list(cyclic = TRUE, year = FALSE))
+  expect_true("date.year" %nin% train_pipeop(po, inputs = list(task))$output$feature_names)
+})
+
+test_that("PipeOpDateFeatures - only year and cyclic", {
+  dat = iris
+  set.seed(1)
+  dat$date = sample(seq(as.POSIXct("2020-01-31"), to = as.POSIXct("2020-03-01"), by = "sec"),
+    size = 150L)
+  task = TaskClassif$new("iris_date", backend = dat, target = "Species")
+  po = PipeOpDateFeatures$new()
+  po$param_set$values$month = TRUE
+  po$param_set$values$month = FALSE
+  po$param_set$values$week_of_year = FALSE
+  po$param_set$values$day_of_year = FALSE
+  po$param_set$values$day_of_month = FALSE
+  po$param_set$values$day_of_week = FALSE
+  po$param_set$values$hour = FALSE
+  po$param_set$values$minute = FALSE
+  po$param_set$values$second = FALSE
+  po$param_set$values$is_day = FALSE
+  expect_true("date.year" %in% train_pipeop(po, inputs = list(task))$output$feature_names)  
+})
+
+test_that("PipeOpDateFeatures - two POSIXct variables", {
+  dat = iris
+  set.seed(1)
+  dat$date1 = sample(seq(as.POSIXct("2020-01-31"), to = as.POSIXct("2020-03-01"), by = "sec"),
+    size = 150L)
+  dat$date2 = sample(seq(as.POSIXct("2020-02-29"), to = as.POSIXct("2020-04-01"), by = "sec"),
+    size = 150L)
+  task = TaskClassif$new("iris_date", backend = dat, target = "Species")
+  po = PipeOpDateFeatures$new(param_vals = list(keep_date_var = TRUE, cyclic = TRUE))
+  expect_identical(train_pipeop(po, inputs = list(task))$output$feature_names,
+    c("Petal.Length", "Petal.Width", "Sepal.Length", "Sepal.Width", "date1", "date2",
+      c(paste0(rep(c("date1.", "date2."), each = 10L),
+          c("year", "month", "week_of_year", "day_of_year", "day_of_month", "day_of_week",
+            "hour", "minute", "second", "is_day")),
+        paste0(rep(c("date1.", "date2."), each = 16L),
+          c("month_sin", "month_cos", "week_of_year_sin", "week_of_year_cos",
+            "day_of_year_sin", "day_of_year_cos", "day_of_month_sin", "day_of_month_cos",
+            "day_of_week_sin", "day_of_week_cos",
+            "hour_sin", "hour_cos", "minute_sin", "minute_cos", "second_sin", "second_cos"))
+      )
+    )
+  )
 })
