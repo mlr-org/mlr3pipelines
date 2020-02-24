4c48a775c861af6e60ec4d6a9f083394feeb2ecf
sumny <lennart.sch@web.de>
2020-02-24 20:34:22 +0100

    add helper functions for leap years and number of weeks, more detailed documentation on max_x + 1, use unlist instead of do.call

--- R/PipeOpDateFeatures.R
+++ R/PipeOpDateFeatures.R
@@ -37,7 +37,7 @@
 #' * `keep_date_var` :: `logical(1)`\cr
 #'   Should the `POSIXct` columns be kept as features? Default FALSE.
 #' * `cyclic` :: `logical(1)`\cr
-#'   Should cyclic features be computed? See Internals? Default FALSE.
+#'   Should cyclic features be computed? See Internals. Default FALSE.
 #' * `year` :: `logical(1)`\cr
 #'   Should the year be extracted as a feature? Default TRUE.
 #' * `month` :: `logical(1)`\cr
@@ -63,11 +63,14 @@
 #' @section Internals:
 #' If `cyclic = TRUE`, cyclic features are computed for the features `"month"`, `"week_of_year"`,
 #' `"day_of_year"`, `"day_of_month"`, `"day_of_week"`, `"hour"`, `"minute"` and `"second"`. This
-#' means that for each feature, `x`, two additional features are computed, namely the sine and
-#' cosine transformation of `2 * pi * x / max_x + 1`. This is useful to respect the cyclical nature
-#' of features such as seconds, i.e., second 21 and second 22 are one second apart, but so are
-#' second 60 and second 01. The transformation always assumes that `min_x = 0`, therefore prior
-#' shifting the values internally by minus one may occur if necessary.
+#' means that for each feature `x`, two additional features are computed, namely the sine and cosine
+#' transformation of `2 * pi * x / max_x + 1` (here `max_x` is the largest possible value the
+#' feature could take on, assuming the lowest possible value is given by 0, e.g., for hours from 0
+#' to 23, this is 23; finally, to then get the total number of possible values `+ 1` is needed).
+#' This is useful to respect the cyclical nature of features such as seconds, i.e., second 21 and
+#' second 22 are one second apart, but so are second 60 and second 1 of the next minute. The
+#' transformation always assumes that `min_x = 0`, therefore prior shifting the values internally by
+#' minus one may occur if necessary.
 #'
 #' @section Methods:
 #' Only methods inherited from [`PipeOpTaskPreprocSimple`]/[`PipeOp`].
@@ -184,28 +187,40 @@ compute_date_features = function(dates, features) {
 # helper function to compute cyclic date features of date features, i.e.,
 # sine and cosine transformations of 2 * pi * x / max_x + 1
 compute_cyclic_date_features = function(date_features, features, date_var) {
-  do.call(c, args = lapply(features, FUN = function(feature) {
+  # drop the date_var-specific colnames here, this makes it easier in lapply
+  column_names = colnames(date_features)
+  colnames(date_features) = c(column_names[1L],
+    gsub(paste0(date_var, "."), replacement = "", x = column_names[-1L]))
+  unlist(lapply(features, FUN = function(feature) {
     # all values are expected to start at 0 and therefore may be shifted by - 1
     value = if (feature %in% c("month", "week_of_year", "day_of_year", "day_of_month")) {
-      date_features[[paste0(date_var, ".", feature)]] - 1L
+      date_features[[feature]] - 1L
     } else {
-      date_features[[paste0(date_var, ".", feature)]]
+      date_features[[feature]]
     }
     maximum = switch(feature,
       month = 12L,
-      week_of_year = 52L,
-      day_of_year = (365L + as.integer((date_features[[paste0(date_var, ".", "year")]] %% 400L == 0L) |
-        (date_features[[paste0(date_var, ".", "year")]] %% 4L == 0L &
-        date_features[[paste0(date_var, ".", "year")]] %% 100L != 0L))),
-      day_of_month = get_days_per_month(date_features[[paste0(date_var, ".", "year")]],
-        month = date_features[[paste0(date_var, ".", "month")]]),
+      week_of_year = get_weeks_per_year(date_features[["year"]]),
+      day_of_year = (365L + as.integer(is_leap_year(date_features[["year"]]))),
+      day_of_month = get_days_per_month(date_features[["year"]], month = date_features[["month"]]),
       day_of_week = 7L,
       hour = 24L,
       minute = 60L,
       second = 60L)
     value_scaled = 2L * pi * value / maximum
     list(sin(value_scaled), cos(value_scaled))
-  }))
+  }), recursive = FALSE)
+}
+
+# helper function to get the number of weeks per year, this can sometimes be 53 instead of 52 (works
+# also vectorized)
+get_weeks_per_year = function(year) {
+  as.numeric(format(as.POSIXct(paste0(year, "-12-31"), format = c("%Y-%m-%d")), "%U"))
+}
+
+# helper function to check whether the year is a leap year (works also vectorized)
+is_leap_year = function(year) {
+  (year %% 4L == 0L & year %% 100L != 0L) | (year %% 400L == 0L)
 }
 
 # helper function to get the number of days per month respecting leap years
@@ -218,7 +233,7 @@ get_days_per_month = function(year, month) {
     if (is.na(year)) {
       return(NA) # early exit if NA
     }
-    if (((year %% 4L == 0L && year %% 100L != 0L) || (year %% 400L == 0L)) && month == 2) {
+    if (is_leap_year(year) && month == 2L) {
       return(29L) # early exit if leap year and February
     }
     days_per_month[month]
