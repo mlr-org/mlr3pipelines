1647e0c3640cc68565ff6e74594ce897e375e8b1
pfistfl <pfistererf@googlemail.com>
2020-03-04 10:46:23 +0100

    no ranger

--- tests/testthat/test_pipeop_missind.R
+++ tests/testthat/test_pipeop_missind.R
@@ -97,7 +97,6 @@ test_that("PipeOpMissInd", {
 
 # https://stackoverflow.com/questions/60512348/how-to-impute-data-with-mlr3-and-predict-with-na-values
 test_that("union with missing rows", {
-  library(mlr3learners)
   data("mtcars", package = "datasets")
   data = mtcars[, 1:3]
   # Train task
@@ -108,26 +107,17 @@ test_that("union with missing rows", {
 
   imp_missind = po("missind")
   imp_num =  po("imputehist", param_vals = list(affect_columns = selector_type("numeric")))
-  learner = lrn("regr.ranger")
+  learner = lrn("regr.rpart")
 
-  g1 = gunion(list(imp_num,imp_missind)) %>>%
-    po("featureunion") %>>% po(learner)
+  g1 = gunion(list(imp_num, imp_missind)) %>>% po("featureunion")
+  g1$train(task_mtcars)
+  out = g1$predict(TaskRegr$new("t2", data2, target = "mpg"))[[1]]$data()
+  assert_true(!any(is.na(out)))
 
-  gl = GraphLearner$new(g1)
-  gl$train(task_mtcars)
-  out = gl$predict_newdata(data2)
-  expect_prediction(out)
-
-
-  g2 = gunion(list(imp_missind, imp_num)) %>>%
-    po("featureunion") %>>% po(learner)
-
-  data = task_mtcars$data()[12:12,]
-  data[1:1, cyl:=NA]
-  gl = GraphLearner$new(g2)
-  gl$train(task_mtcars)
-  out = gl$predict_newdata(data2)
-  expect_prediction(out)
+  g2= gunion(list(imp_missind, imp_num)) %>>% po("featureunion")
+  g2$train(task_mtcars)
+  out = g2$predict(TaskRegr$new("t2", data2, target = "mpg"))[[1]]$data()
+  assert_true(!any(is.na(out)))
 })
 
 # https://stackoverflow.com/questions/60512348/how-to-impute-data-with-mlr3-and-predict-with-na-values
