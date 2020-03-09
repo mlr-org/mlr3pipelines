9f9a618188510262d5cabc23f6776323ee6099b3
sumny <lennart.sch@web.de>
2020-03-09 17:32:17 +0100

    finish first draft

--- R/PipeOpColRoles.R
+++ R/PipeOpColRoles.R
@@ -2,60 +2,52 @@
 #'
 #' @usage NULL
 #' @name mlr_pipeops_colroles
-#' @format [`R6Class`] object inheriting from [`PipeOpTaskPreprocSimple`]/[`PipeOp`].
+#' @format [`R6Class`] object inheriting from [`PipeOpTaskPreprocSimple`]/[`PipeOpTaskPreproc`]/[`PipeOp`].
 #'
 #' @description
-#' ColRoless a [`Task`][mlr3::Task] to use a fraction of the rows.
-#'
-#' Sampling happens only during training phase. Subsampling a [`Task`][mlr3::Task] may be
-#' beneficial for training time at possibly (depending on original [`Task`][mlr3::Task] size)
-#' negligible cost of predictive performance.
+#' Changes the columns roles of the input [`Task`][mlr3::Task] according to `new_role`.
 #'
 #' @section Construction:
 #' ```
 #' PipeOpColRoles$new(id = "colroles", param_vals = list())
 #' ```
-#' * `id` :: `character(1)`
-#'   Identifier of the resulting  object, default `"colroles"`
+#'
+#' * `id` :: `character(1)`\cr
+#'   Identifier of resulting object, default `"colroles"`.
 #' * `param_vals` :: named `list`\cr
-#'   List of hyperparameter settings, overwriting the hyperparameter settings that would otherwise be set during construction. Default `list()`.
+#'   List of hyperparameter settings, overwriting the hyperparameter settings that would otherwise
+#'   be set during construction. Default `list()`.
 #'
 #' @section Input and Output Channels:
-#' Input and output channels are inherited from [`PipeOpTaskPreprocSimple`].
+#' Input and output channels are inherited from [`PipeOpTaskPreproc`].
 #'
-#' The output during training is the input [`Task`][mlr3::Task] with added or removed rows according to the sampling.
-#' The output during prediction is the unchanged input.
+#' The output is the input [`Task`][mlr3::Task] with transformed column roles according to `new_role`.
 #'
 #' @section State:
-#' The `$state` is a named `list` with the `$state` elements inherited from [`PipeOpTaskPreprocSimple`].
+#' The `$state` is a named `list` with the `$state` elements inherited from [`PipeOpTaskPreproc`].
 #'
 #' @section Parameters:
-#' The parameters are the parameters inherited from [`PipeOpTaskPreprocSimple`]; however, the `affect_columns` parameter is *not* present. Further parameters are:
-#' * `frac` :: `numeric(1)`\cr
-#'   Fraction of rows in the [`Task`][mlr3::Task] to keep. May only be greater than 1 if `replace` is `TRUE`. Initialized to `(1 - exp(-1)) == 0.6321`.
-#' * `stratify` :: `logical(1)`\cr
-#'   Should the colroless be stratified by target? Initialized to `FALSE`. May only be `TRUE` for [`TaskClassif`][mlr3::TaskClassif] input.
-#' * `replace` :: `logical(1)`\cr
-#'   Sample with replacement? Initialized to `FALSE`.
-#'
-#' @section Internals:
-#' Uses `task$filter()` to remove rows. If `replace` is `TRUE` and identical rows are added, then the `task$row_roles$use` can *not* be used
-#' to duplicate rows because of \[inaudible\]; instead the `task$rbind()` function is used, and
-#' a new [`data.table`] is attached that contains all rows that are being duplicated exactly as many times as they are being added.
-#'
-#' @section Fields:
-#' Only fields inherited from [`PipeOpTaskPreprocSimple`]/[`PipeOp`].
+#' The parameters are the parameters inherited from [`PipeOpTaskPreproc`], as well as:
+#' * `new_role`  :: `list` \cr
+#'   Named list of new column roles. The names must match the column names of the input task that
+#'   will later be trained on. Each entry of the list must contain a character vector with possible
+#'   values of `feature`, `name`, `order`, `group`, `stratum`,  and `weight`. If the value is given
+#'   as `character()`, the column will be dropped from the input task. Changing the role of a column
+#'   results in this column loosing its previous role(s). Setting a new target variable or changing
+#'   the role of an existing target variable is not supported. 
 #'
 #' @section Methods:
-#' Only methods inherited from [`PipeOpTaskPreprocSimple`]/[`PipeOp`].
+#' Only methods inherited from [`PipeOpTaskPreprocSimple`]/[`PipeOpTaskPreproc`]/[`PipeOp`].
 #'
 #' @examples
 #' library("mlr3")
 #'
-#' pos = mlr_pipeops$get("colroles", param_vals = list(frac = 0.7, stratify = TRUE))
+#' task = tsk("boston_housing")
+#' pop = po("colroles", param_vals = list(
+#'   new_role = list(cmedv = "order")
+#' ))
 #'
-#' pos$train(list(tsk("iris")))
-#'
+#' pop$train(list(task))
 #' @family PipeOps
 #' @include PipeOpTaskPreproc.R
 #' @export
@@ -64,28 +56,37 @@ PipeOpColRoles = R6Class("PipeOpColRoles",
   public = list(
     initialize = function(id = "colroles", param_vals = list()) {
       ps = ParamSet$new(params = list(
-        # vector of column names
-        ParamUty$new("column", tags = c("train", "predict")),
-        # list of length length(column) with a vector of roles
-        ParamUty$new("new_role", tags = c("train", "predict"))
+        # named list, each entry with a vector of roles
+        ParamUty$new("new_role", tags = c("train", "predict"), custom_check = function(x) {
+          first_check = check_list(x, types = "character", any.missing = FALSE, min.len = 1L, names = "named")
+          if (is.character(first_check)) {
+            return(first_check)
+          }
+          # for changing the target use PipeOpNewTarget
+          # a value of "character()" will lead to this column being dropped
+          check_subset(unlist(x), c("feature", "name", "order", "group", "stratum", "weight"))
+        })
       ))
-      #ps$values = list(frac = 1 - exp(-1), stratify = FALSE, replace = FALSE)
       super$initialize(id, param_set = ps, param_vals = param_vals, can_subset_cols = FALSE)
     },
 
-    #get_state = function(task) {
-    #  list(column = self$param_set$values$column, new_role = self$param_set$values$new_role)
-    #},
-
     transform = function(task) {
+      # names of "new_role" must be a subset of the column names of the task
+      assert_subset(names(self$param_set$values$new_role), choices = task$col_info$id)
+      # changing the role of a target is not supported
+      if (any(task$col_roles$target %in% names(self$param_set$values$new_role))) {
+        stop("Cannot change the role of a target.")
+      }
       col_roles = unlist(task$col_roles)
       indices = rep.int(seq_along(task$col_roles), times = unlist(map(task$col_roles, length)))
-      for(i in seq_along(self$param_set$values$column)) {
-        column = self$param_set$values$column[i]
+      for(i in seq_along(self$param_set$values$new_role)) {
+        column = names(self$param_set$values$new_role)[i]
         old_role = indices[match(column, col_roles)]
+        # first remove any old role
         for(role in old_role) {
           task$col_roles[[role]] = setdiff(task$col_roles[[role]], column)
         }
+        # then add the new role(s)
         if (length(self$param_set$values$new_role[[i]])) {
           new_role = match(self$param_set$values$new_role[[i]], names(task$col_roles))
           for(role in new_role) {
@@ -93,7 +94,6 @@ PipeOpColRoles = R6Class("PipeOpColRoles",
           }
         }
       }
-      
       task
     }
   )
