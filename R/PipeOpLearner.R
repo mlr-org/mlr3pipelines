bf7d3632376ca89d93e5370cb6b6fc1d7daecb89
sumny <lennart.sch@web.de>
2020-04-02 12:30:21 +0200

    use clone = TRUE for learners

--- R/PipeOpLearner.R
+++ R/PipeOpLearner.R
@@ -79,7 +79,7 @@
 PipeOpLearner = R6Class("PipeOpLearner", inherit = PipeOp,
   public = list(
     initialize = function(learner, id = if (is.character(learner)) learner else learner$id, param_vals = list()) {
-      private$.learner = as_learner(learner)$clone(deep = TRUE)  # FIXME: use `clone=TRUE` when mlr-org/mlr3#344 is fixed
+      private$.learner = as_learner(learner, clone = TRUE)
       task_type = mlr_reflections$task_types[private$.learner$task_type]$task
       out_type = mlr_reflections$task_types[private$.learner$task_type]$prediction
       super$initialize(id, param_set = alist(private$.learner$param_set), param_vals = param_vals,
