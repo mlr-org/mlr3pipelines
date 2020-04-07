77ca13a633504bb745ec8ae6b57619da81c08ec6
pfistfl <pfistererf@googlemail.com>
2020-04-07 12:17:21 +0200

    add retrafoless target trafo

--- R/PipeOpTrafo.R
+++ R/PipeOpTrafo.R
@@ -488,3 +488,120 @@ PipeOpTargetTrafoScaleRange = R6Class("PipeOpTargetTrafoScaleRange",
 )
 
 mlr_pipeops$add("targettrafoscalerange", PipeOpTargetTrafoScaleRange)
+
+
+#' @title PipeOpUpdateTarget
+#' @usage NULL
+#' @name mlr_pipeops_updatetarget
+#' @format Abstract [`R6Class`] inheriting from [`PipeOp`].
+#'
+#' @description
+#' Handles target transformation operations that do not need explicit inversion.
+#' In case the new target is required during predict, creates a vector of `NA`.
+#' Works similar to [`PipeOpTargetTrafo`] and [`PipeOpTargetTrafoSimple`], but forgoes the
+#' inversion step.
+#' In case target after the `trafo` is a factor, levels are saved to `$state`.\cr 
+#' 
+#' During prediction: Sets all target values to `NA` before calling the `trafo` again.
+#' In case target after the `trafo` is a factor, levels saved in the `state` are
+#' set during prediction.
+#' 
+#'
+#' @section Construction:
+#' ```
+#' PipeOpUpdateTarget$new(id, param_set = ParamSet$new(),
+#'   param_vals = list(), packages = character(0))
+#' ```
+#'
+#' * `id` :: `character(1)`\cr
+#'   Identifier of resulting object. See `$id` slot of [`PipeOp`].
+#' * `param_vals` :: named `list`\cr
+#'   List of hyperparameter settings, overwriting the hyperparameter settings given in `param_set`.
+#'   The subclass should have its own `param_vals` parameter and pass it on to `super$initialize()`.
+#'   Default `list()`.
+#'
+#' @section Parameters:
+#' The parameters are the parameters inherited from [`PipeOpTargetTrafo`], as well as:
+#' * `trafo` :: `function`\cr
+#'   Transformation function for the target. Should only be a function of the target, i.e., taking a
+#'   single argument. Default is `identity`.
+#' * `new_target_name` :: `character(1)`\cr
+#'   Optionally give the transformed target a new name. By default the original name is used.
+#' * `new_task_type` :: `character(1)`\cr
+#'   Optionally a new task type can be set. Legal types are listed in
+#'   `mlr_reflections$task_types$type`.
+#' 
+#' @section State:
+#' The `$state` is a list of class levels for each target after trafo. 
+#' `list(0)` if none of the targets have levels.
+#' 
+#' @section Methods:
+#' Only methods inherited from [`PipeOp`].
+#' 
+#'
+#' @family mlr3pipelines backend related
+#' @family PipeOps
+#' @include PipeOp.R
+#' @examples
+#'   # Create a binary class task from iris
+#'   trafo_fun = function(x) {factor(ifelse(x == "setosa", "setosa", "other"))}
+#'   po = PipeOpUpdateTarget$new(param_vals = list(trafo = trafo_fun, new_target_name = "setosa"))
+#'   po$train(list(tsk("iris")))
+#'   po$predict(list(tsk("iris")))
+#' @export
+PipeOpUpdateTarget = R6Class("PipeOpUpdateTarget",
+  inherit = PipeOp,
+  public = list(
+    initialize = function(id = "update_target", param_vals = list()) {
+      ps = ParamSet$new(params = list(
+        ParamUty$new("trafo", default = identity, tags = c("train", "predict"), custom_check = function(x) check_function(x, nargs = 1L)),
+        ParamUty$new("new_target_name", tags = c("train", "predict"), custom_check = function(x) check_character(x, any.missing = FALSE, len = 1L)),
+        ParamUty$new("new_task_type", tags = c("train", "predict"), custom_check = function(x) check_choice(x, choices = mlr_reflections$task_types$type))
+        )
+      )
+      ps$values = list(trafo = identity)
+      super$initialize(id = id, param_set = ps, param_vals = param_vals,
+        input =  data.table(name = "input",  train = "Task", predict = "Task"),
+        output = data.table(name = "output", train = "Task", predict = "Task")
+      )
+    }
+  ),
+  private = list(
+    .train = function(inputs) {
+      intask = inputs[[1]]$clone(deep = TRUE)
+      # Apply fun to target, rename, cbind and convert task if required
+      new_target = data.table(self$param_set$values$trafo(intask$data(cols = intask$target_names)))
+      setnames(new_target, colnames(new_target), self$param_set$values$new_target_name)
+      self$state = keep(map(new_target, levels), Negate(is.null))
+      intask$cbind(new_target)
+      list(private$.update_target(intask))
+    },
+
+    .predict = function(inputs) {
+      intask = inputs[[1]]$clone(deep = TRUE)
+      # During predict, we set the new target to NA and then call the trafo
+      new_target = set(intask$data(cols = intask$target_names), j = intask$target_names, value = NA)
+      new_target = data.table(self$param_set$values$trafo(new_target))
+      # rename, cbind and convert
+      setnames(new_target, colnames(new_target), self$param_set$values$new_target_name)
+      # Make sure levels match target levels
+      if (length(self$state))
+        new_target = imap_dtc(new_target, function(x, nms) {
+          if(nms %in% names(self$state))
+            levels(x) = self$state[[nms]]
+          return(x)
+      })
+      intask$cbind(new_target)
+      list(private$.update_target(intask))
+    },
+
+    # updates the target of a task and also the task_type (if needed), uses convert_task
+    .update_target = function(task, ...) {
+      convert_task(task, new_target = self$param_set$values$new_target_name,
+        new_type = self$param_set$values$new_task_type, drop_original_target = TRUE, ...)
+    }
+  )
+)
+
+
+mlr_pipeops$add("update_target", PipeOpUpdateTarget)
---
+++ tests/testthat/test_pipeop_updatetarget.R
@@ -0,0 +1,53 @@
+ context("PipeOpUpdateTarget")
+
+test_that("update target multi to binary", {
+  trafo_fun = function(x) {factor(ifelse(x == "setosa", "setosa", "other"))}
+  pom = po("update_target", param_vals = list(trafo = trafo_fun, new_target_name = "setosa"))
+  expect_pipeop(pom)
+  newtsk = pom$train(list(tsk("iris")))[[1]]
+  expect_task(newtsk)
+  expect_true("setosa" %in% newtsk$target_names)
+  expect_true(all((newtsk$data()$setosa == "setosa") == (tsk("iris")$data()$Species == "setosa")))
+  expect_true(pom$is_trained)
+
+  newtsk2 = pom$predict(list(tsk("iris")))[[1]]
+  expect_task(newtsk2)
+  expect_true("setosa" %in% newtsk2$target_names)
+  expect_true(all(is.na(newtsk2$data()$setosa)))
+  expect_true(all(levels(newtsk2$data()$setosa) == c("other", "setosa")))
+})
+
+test_that("update target regr to classif", {
+  trafo_fun = function(x) {factor(ifelse(x < 25, "<25", ">=25"))}
+  pom = po("update_target", param_vals = list(trafo = trafo_fun, new_target_name = "threshold_25",
+    new_task_type = "classif"))
+  expect_pipeop(pom)
+  newtsk = pom$train(list(tsk("boston_housing")))[[1]]
+  expect_task(newtsk)
+  expect_true("threshold_25" %in% newtsk$target_names)
+  expect_true(all((newtsk$data()$threshold_25 == "<25") == (tsk("boston_housing")$data()$medv < 25)))
+  expect_true(pom$is_trained)
+
+  newtsk2 = pom$predict(list(tsk("boston_housing")))[[1]]
+  expect_task(newtsk2)
+  expect_true("threshold_25" %in% newtsk2$target_names)
+  expect_true(all(is.na(newtsk2$data()$threshold_25)))
+  expect_true(all(levels(newtsk2$data()$threshold_25) == c("<25", ">=25")))
+})
+
+test_that("update target classif to regr", {
+  trafo_fun = function(x) {map_dtc(x, as.numeric)}
+  pom = po("update_target", param_vals = list(trafo = trafo_fun, new_target_name = "quality",
+    new_task_type = "regr"))
+  expect_pipeop(pom)
+  newtsk = pom$train(list(tsk("wine")))[[1]]
+  expect_task(newtsk)
+  expect_true("quality" %in% newtsk$target_names)
+  expect_true(all(newtsk$data()$quality == as.numeric(tsk("wine")$data()$type)))
+  expect_true(pom$is_trained)
+
+  newtsk2 = pom$predict(list(tsk("wine")))[[1]]
+  expect_task(newtsk2)
+  expect_true("quality" %in% newtsk2$target_names)
+  expect_true(all(is.na(newtsk2$data()$quality)))
+})
