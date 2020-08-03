4652ef9e482db06aea5ef53d90bb5468a7e6f9e1
mb706 <mlr.developer@mb706.com>
2020-08-03 21:39:28 +0200

    fix missind test: affect_columns must be NULL

--- tests/testthat/test_pipeop_missind.R
+++ tests/testthat/test_pipeop_missind.R
@@ -28,16 +28,16 @@ test_that("PipeOpMissInd", {
   task_no_lgl = TaskClassif$new("mdata", as_data_backend(mdata), target = "l")
 
   expect_datapreproc_pipeop_class(PipeOpMissInd, task = task,
-    constargs = list(param_vals = list(which = "missing_train", type = "logical")))
+    constargs = list(param_vals = list(which = "missing_train", type = "logical", affect_columns = NULL)))
 
   expect_datapreproc_pipeop_class(PipeOpMissInd, task = task,
-    constargs = list(param_vals = list(which = "all", type = "logical")))
+    constargs = list(param_vals = list(which = "all", type = "logical", affect_columns = NULL)))
 
   expect_datapreproc_pipeop_class(PipeOpMissInd, task = task,
-    constargs = list(param_vals = list(which = "all", type = "factor")))
+    constargs = list(param_vals = list(which = "all", type = "factor", affect_columns = NULL)))
 
   expect_datapreproc_pipeop_class(PipeOpMissInd, task = task,
-    constargs = list(param_vals = list(which = "all", type = "numeric")))
+    constargs = list(param_vals = list(which = "all", type = "numeric", affect_columns = NULL)))
 
   po = PipeOpMissInd$new(param_vals = list(which = "all", type = "logical"))
 
