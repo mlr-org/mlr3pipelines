b4abe333e76d20fe6fb142962b2cd63f5af9a591
mb706 <mlr.developer@mb706.com>
2020-08-03 21:51:36 +0200

    fix missind tests

--- tests/testthat/test_pipeop_missind.R
+++ tests/testthat/test_pipeop_missind.R
@@ -39,7 +39,7 @@ test_that("PipeOpMissInd", {
   expect_datapreproc_pipeop_class(PipeOpMissInd, task = task,
     constargs = list(param_vals = list(which = "all", type = "numeric", affect_columns = NULL)))
 
-  po = PipeOpMissInd$new(param_vals = list(which = "all", type = "logical"))
+  po = PipeOpMissInd$new(param_vals = list(which = "all", type = "logical", affect_columns = NULL))
 
   task_trained = po$train(list(task$clone(deep = TRUE)$filter(5:6)))[[1]]$data()
 
