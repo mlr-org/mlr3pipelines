f1e0c6f8ef02e9264aee5a7129cc853b2d3f94f0
sumny <lennart.sch@web.de>
2020-11-01 22:43:33 +0100

    assert input in unlist_multiplicities

--- R/PipeOp.R
+++ R/PipeOp.R
@@ -449,6 +449,7 @@ multiplicity_type_nesting_level = function(str, varname) {
 # @param poid: `character(1)`: character id of the PipeOp
 # @return `list`
 unpack_multiplicities = function(input, expected_nesting_level, inputnames, poid) {
+  assert_list(input)
   unpacking = mapply(multiplicity_nests_deeper_than, input, expected_nesting_level)
   if (!any(unpacking)) {
     return(NULL)  # no unpacking
