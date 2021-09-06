f39f3d4279805f2db7db4c47ea0f86e767bf9e1b
Patrick Schratz <patrick.schratz@gmail.com>
2021-09-06 21:35:05 +0200

    GHA: install {Biobase} dep for {NMF} (#594)
    * install Biobase for NMF
    
    * add_step
    
    * before install

--- tic.R
+++ tic.R
@@ -1,6 +1,11 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
+# dependency of NMF
+# installing from GH avoids using a static Bioconductor version
+get_stage("before_install") %>%
+  add_step(step_install_github("Bioconductor/Biobase"))
+
 if (ci_on_ghactions() && identical(ci_get_env("MLR3"), "devel")) {
   get_stage("install") %>%
     add_step(step_install_github("mlr-org/mlr3"))
@@ -14,5 +19,3 @@ if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   do_pkgdown()
 }
 
-get_stage("after_success") %>%
-  add_code_step(system("curl -s https://raw.githubusercontent.com/mlr-org/mlr3orga/master/trigger-mlr3book.sh | bash"))
