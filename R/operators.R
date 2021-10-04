ca92ec25f72f9246a703fc665ff8f4e0eaaf3527
mb706 <mlr.developer@mb706.com>
2021-10-04 16:11:28 +0200

    alternative to chain_graphs

--- R/operators.R
+++ R/operators.R
@@ -38,12 +38,34 @@
 #' Note that if `g1` is `NULL`, `g2` converted to a [`Graph`] will be returned.
 #' Analogously, if `g2` is `NULL`, `g1` converted to a [`Graph`] will be returned.
 #'
+#' @section Chaining Graphs:
+#' `concat_graphs` can also be called with the `glist` argument, which takes an arbitrary amount of [`Graph`]s or [`PipeOp`]s (or objects that can be automatically
+#' converted into [`Graph`]s or [`PipeOp`]s) and joins them in a serial [`Graph`], as if connecting them using [`%>>%`].
+#'
+#' Care is taken to avoid unnecessarily cloning of components. A call of
+#' `chain_graphs(list(g1, g2, g3, g4, ...), in_place = FALSE)` is equivalent to
+#' `g1 %>>% g2 %>>>% g3 %>>>% g4 %>>>% ...`.
+#' A call of `chain_graphs(list(g1, g2, g3, g4, ...), in_place = FALSE)`
+#' is equivalent to `g1 %>>>% g2 %>>>% g3 %>>>% g4 %>>>% ...` (differing in the
+#' first operator being `%>>>%` as well).
+#'
+#' `concat_graphs(glist = <list>)` (implicitly with `in_place = FALSE`) is a safe way of generating large linear pipelines quickly, while
+#' still avoiding to change any of its inputs by reference, and avoiding the risk of ending up with broken objects.
+#'
 #' @param g1 ([`Graph`] | [`PipeOp`] | [`Learner`][mlr3::Learner] | [`Filter`][mlr3filters::Filter] | `list` | `...`) \cr
 #'   [`Graph`] / [`PipeOp`] / object-convertible-to-[`PipeOp`] to put in front of `g2`.
 #' @param g2 ([`Graph`] | [`PipeOp`] | [`Learner`][mlr3::Learner] | [`Filter`][mlr3filters::Filter] | `list` | `...`) \cr
 #'   [`Graph`] / [`PipeOp`] / object-convertible-to-[`PipeOp`] to put after  `g1`.
+#' @param glist `list` of ([`Graph`] | [`PipeOp`] | [`Learner`][mlr3::Learner] | [`Filter`][mlr3filters::Filter] | `list` | `...`)\cr
+#'   List of elements which are the [`Graph`]s to be joined. Elements must be convertible to [`Graph`] or [`PipeOp`] using [`as_graph()`] and [`as_pipeop()`].
+#'   `NULL` is the neutral element of [`%>>%`] and skipped. When this is given, `g1` and `g2` must not be given.
 #' @param in_place (`logical(1)`)\cr
-#'   Whether to try to avoid cloning `g1`. If `g1` is not a [`Graph`], then it is cloned regardless.
+#'   When `g1` and `g2` are given: Whether to try to avoid cloning `g1`. If `g1` is not a [`Graph`], then it is cloned regardless.\n
+#'   When `glist` is given instead:
+#'   Whether to try to avoid cloning the first element of `glist`, similar to the difference
+#'   of `%>>>%` over `%>>%`. This can only be avoided if `glist[[1]]` is already a [`Graph`].
+#'   Beware that, when `in_place` is `TRUE` and if `concat_graphs()` fails because of id collisions, then `glist[[1]]` will possibly be in an incompletely
+#'   modified state.
 #'
 #' @return [`Graph`]: the constructed [`Graph`].
 #' @family Graph operators
@@ -81,7 +103,11 @@
 #' o1 %>>>% o2
 #'
 #' o1  # not changed, becuase not a Graph.
-concat_graphs = function(g1, g2, in_place = FALSE) {
+concat_graphs = function(g1, g2, glist, in_place = FALSE) {
+  if (!missing(glist)) {
+    if (!missing(g1) || !missing(g2)) stop("When glist is given, g1 and g2 must not be given")
+    return(chain_graphs(graphs = glist, in_place = in_place))
+  }
   assert_flag(in_place)
   # neutral elements handling
   if (is.null(g1)) return(if (!is.null(g2)) as_graph(g2, clone = TRUE))
@@ -94,7 +120,6 @@ concat_graphs = function(g1, g2, in_place = FALSE) {
   if (nrow(g1out) != 1 && nrow(g1out) != nrow(g2in) && !(nrow(g2in) == 1 && g2in$channel.name == "...")) {
     stopf("Graphs / PipeOps to be connected have mismatching number of inputs / outputs.")
   }
-  g = gunion(list(g1, g2), in_place = in_place)
 
 
   # check that types agree
@@ -114,6 +139,8 @@ concat_graphs = function(g1, g2, in_place = FALSE) {
   # build edges from free output channels of g1 and free input channels of g2
   new_edges = cbind(g1out[, list(src_id = get("op.id"), src_channel = get("channel.name"))],
     g2in[, list(dst_id = get("op.id"), dst_channel = get("channel.name"))])
+
+  g = gunion(list(g1, g2), in_place = in_place)
   g$edges = rbind(g$edges, new_edges)
   g
 }
@@ -158,7 +185,7 @@ strip_multiplicity_type = function(type) {
 #'   Beware that, if `chain_graphs()` fails because of id collisions, then `graphs[[1]]` will possibly be in an incompletely
 #'   modified state when `in_place` is `TRUE`.
 #' @return [`Graph`] the resulting [`Graph`], or `NULL` if there are no non-null values in `graphs`.
-#'
+#' @export
 chain_graphs = function(graphs, in_place = FALSE) {
   assert_list(graphs)
   graphs = discard(graphs, is.null)
