9ade0185033752ea353d007c5eb512a868fa8500
mb706 <mlr.developer@mb706.com>
2023-05-22 01:14:40 +0200

    I think we don't need special data table vars here

--- R/Graph.R
+++ R/Graph.R
@@ -623,7 +623,7 @@ graph_reduce = function(self, input, fun, single_input) {
       op$.result = output
     }
 
-    edges[list(id, op$output$name), "payload" := list(..output[get("src_channel")]), on = c("src_id", "src_channel")]
+    edges[list(id, op$output$name), "payload" := list(output[get("src_channel")]), on = c("src_id", "src_channel")]
   }
 
   # get payload of edges that go to terminal node.
