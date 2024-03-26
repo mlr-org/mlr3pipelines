47e3ee5a0af9e544230208c7dcc7d1ae04ee6caa
mb706 <mlr.developer@mb706.com>
2024-03-26 17:57:54 +0100

    adapt tests

--- tests/testthat/test_mlr_graphs_stacking.R
+++ tests/testthat/test_mlr_graphs_stacking.R
@@ -10,7 +10,7 @@ test_that("Stacking Pipeline", {
   # default
   graph_stack = pipeline_stacking(base_learners, super_learner)
   expect_graph(graph_stack)
-  expect_names(graph_stack$ids(), identical.to = c("base.rpart", "nop", "featureunion", "super.rpart"))
+  expect_names(graph_stack$ids(), identical.to = c("base.rpart", "nop", "featureunion_stacking", "super.rpart"))
   graph_learner = as_learner(graph_stack)
   graph_learner$train(tsk("iris"))
   expect_class(graph_learner$model$super.rpart$model, "rpart")
@@ -19,7 +19,7 @@ test_that("Stacking Pipeline", {
   # no nop
   graph_stack = pipeline_stacking(base_learners, super_learner, use_features = FALSE)
   expect_graph(graph_stack)
-  expect_names(graph_stack$ids(), identical.to = c("base.rpart", "featureunion", "super.rpart"))
+  expect_names(graph_stack$ids(), identical.to = c("base.rpart", "featureunion_stacking", "super.rpart"))
   graph_learner = as_learner(graph_stack)
   graph_learner$train(tsk("iris"))
   expect_class(graph_learner$model$super.rpart$model, "rpart")
@@ -28,7 +28,7 @@ test_that("Stacking Pipeline", {
   # folds
   graph_stack = pipeline_stacking(base_learners, super_learner, folds = 5)
   expect_graph(graph_stack)
-  expect_names(graph_stack$ids(), identical.to = c("base.rpart", "nop", "featureunion", "super.rpart"))
+  expect_names(graph_stack$ids(), identical.to = c("base.rpart", "nop", "featureunion_stacking", "super.rpart"))
   graph_learner = as_learner(graph_stack)
   graph_learner$train(tsk("iris"))
   expect_equal(graph_learner$graph$pipeops$base.rpart$param_set$values$resampling.folds, 5)
@@ -38,7 +38,7 @@ test_that("Stacking Pipeline", {
   # insample
   graph_stack = pipeline_stacking(base_learners, super_learner, method = "insample")
   expect_graph(graph_stack)
-  expect_names(graph_stack$ids(), identical.to = c("base.rpart", "nop", "featureunion", "super.rpart"))
+  expect_names(graph_stack$ids(), identical.to = c("base.rpart", "nop", "featureunion_stacking", "super.rpart"))
   graph_learner = as_learner(graph_stack)
   graph_learner$train(tsk("iris"))
   expect_equal(graph_learner$graph$pipeops$base.rpart$param_set$values$resampling.method, "insample")
