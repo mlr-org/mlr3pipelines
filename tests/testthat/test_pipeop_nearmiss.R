2bf7004f57695ea9dc4b7529a4a8d0f2e94b0f84
kenomersmannPC <advieser@gmail.com>
2024-08-31 14:30:04 +0200

    Added PipeOpNearmiss tests

---
+++ tests/testthat/test_pipeop_nearmiss.R
@@ -0,0 +1,34 @@
+context("PipeOpNearmiss")
+
+test_that("PipeOpNearmiss - basic properties", {
+  skip_if_not_installed("themis")
+
+  task = mlr_tasks$get("wine")
+  expect_datapreproc_pipeop_class(PipeOpNearmiss, task = task, predict_like_train = FALSE)
+
+})
+
+test_that("PipeOpNearmiss - train works as intended", {
+  skip_if_not_installed("themis")
+
+  op = PipeOpNearmiss$new()
+  task = mlr_tasks$get("wine")
+
+  # Compare to themis::nearmiss
+  train_out = op$train(list(task))[[1]]$data()
+  nearmiss_out = setDT(invoke(themis::nearmiss, df = task$data(), var = task$target_names))
+
+  expect_equal(train_out, nearmiss_out)
+
+  # Empty task is returned unchanged
+  task$select(character(0))
+  expect_equal(
+    op$train(list(task))[[1L]],
+    task
+  )
+
+  # PipeOp does not accept tasks with wrong feature types
+  task = tsk("german_credit")
+  expect_error(op$train(list(task)))
+
+})
