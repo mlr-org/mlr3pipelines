32449cb3bbe320094de41153df8c2f98edb6956b
kenomersmannPC <advieser@gmail.com>
2024-10-21 20:19:32 +0200

    corrected test file name + WIP tests for task_filter_ex

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,6 +1,13 @@
 context("utils")
 
-test_that("task_filter_ex", {
+# test on normal task
+# test after select
+# test with group
+# test renaming with group
+# test changed row_roles$use (some removed, duplicates)
+# test with task with trailing rows filtered out
+
+test_that("task_filter_ex - Basic functionality", {
   task = mlr_tasks$get("iris")
 
   rowidx = as.integer(c(1, 2, 3, 2, 1, 2, 3, 2, 1))  # annoying and unnecessary mlr3 type strictness
@@ -12,13 +19,25 @@ test_that("task_filter_ex", {
   tfiltered = task_filter_ex(task$clone(), rowidx)
   expect_equal(tfiltered$data(), task$data(rows = rowidx))
 
-  task$set_col_roles("Petal.Length", "group")
-  tfiltered = task_filter_ex(task$clone(), rowidx)
-  expect_equal(tfiltered$data(), task$data(rows = rowidx))
-  expect_equal(tfiltered$groups$group, task$groups[rowidx]$group)
-
   task$select(character(0))
   tfiltered = task_filter_ex(task$clone(), rowidx)
   expect_equal(tfiltered$data(), task$data(rows = rowidx))
   expect_equal(tfiltered$groups$group, task$groups[rowidx]$group)
+})
+
+test_that("task_filter_ex - task with col role group", {
+  task = mlr_tasks$get("iris")
+  task$cbind(data.frame(grp = sample(LETTERS[1:8], size = task$nrow, replace = TRUE)))
+  task$set_col_roles("grp", "group")
+
+  rowidx = as.integer(c(1, 2, 3, 2, 1, 2, 3, 2, 1))
+
+  tfiltered = task_filter_ex(task$clone(), rowidx)
+  expect_equal(tfiltered$data(), task$data(rows = rowidx))
+  expect_equal(tfiltered$groups$group, task$groups[rowidx]$group)
+
+})
+
+test_that("task_filter_ex - changed row_roles$use", {
+
 })
