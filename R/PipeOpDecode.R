0a8e774973cc8ca3a39a2d9e0a38eae2eb0b7cc2
kenomersmannPC <advieser@gmail.com>
2024-11-26 23:10:56 +0100

    clean up

--- R/PipeOpDecode.R
+++ R/PipeOpDecode.R
@@ -48,7 +48,7 @@ PipeOpDecode = R6Class("PipeOpDecode",
     initialize = function(id = "decode", param_vals = list()) {
       ps = ps(
         treatment_encoding = p_lgl(tags = c("train", "required")),
-        treatment_cutoff = p_dbl(default = 0, tags = "train", requires = quote(treatment_encoding == TRUE)),
+        treatment_cutoff = p_dbl(default = 0, tags = "train", depends = quote(treatment_encoding == TRUE)),
         group_pattern = p_uty(custom_check = check_string, tags = c("train", "required")),
         ties_method = p_fct(c("first", "last", "random"), tags = c("train", "required"))
       )
@@ -62,7 +62,7 @@ PipeOpDecode = R6Class("PipeOpDecode",
       pv = self$param_set$values
       cols = colnames(dt)
 
-      if (treatment_encoding) {
+      if (pv$treatment_encoding) {
         # Determine name for reference level
         ref_name = "ref"
         counter = 1
@@ -72,76 +72,90 @@ PipeOpDecode = R6Class("PipeOpDecode",
         }
       }
 
-      # If pattern == "", all columns are collapsed into one column
-      # If "pipeop.decoded" is already taken, we overwrite it!
+      # If pattern == "", all columns are collapsed into one column.
+      # Note, that column "pipeop.decoded" gets overwritten if it already exists.
       if (pv$group_pattern == "") {
-        if (treatment_encoding) {
-          # First add entry ref_name with empty name
-          cmap = list(pipeop.decoded = c(set_names(cols, cols)))
+        cmap = list(pipeop.decoded = c(set_names(cols, cols)))
+        if (pv$treatment_encoding) {
+          # Append ref_name with empty name (i.e. "")
           cmap[[pipeop.decoded]][[length(cols) + 1]] = ref_name
-          return(list(colmaps = cmap))
         }
-        return(list(colmaps = list(pipeop.decoded = set_names(cols, cols))))
+
+        s = list(
+          colmaps = cmap,
+          treatment_encoding = pv$treatment_encoding,
+          cutoff = pv$treatment_cutoff %??% 0,
+          ties_method = pv$ties_method
+        )
+
+        return(s)
       }
 
-      # Drop columns that do contain group_pattern
-      # What about cols starting with .? -> probably let user exclude this by changing group_pattern are using affect_columns
+      # Drop columns that do not match group_pattern
       cols = cols[grepl(pv$group_pattern, cols, perl = TRUE)]
 
-      # Extract factor names
-      # If group_pattern contains does not contain a capturing group, fcts cannot be generated
-      matches = regmatches(cols, regexec(pv$group_pattern, cols, perl = TRUE))
-      fcts = map_chr(matches, 2)
-
-      if (any(nchar(fcts) == 0)) {
-        stopf("Pattern %s with column %s would produce empty decoded column name", group_pattern, cols[nchar(fcts) == 0])
-      }
-      # Extract level names
+      # Extract names for new levels
       lvls = set_names(gsub(pv$group_pattern, "", cols, perl = TRUE), cols)
 
-      # test that split is consistent for this use case
-      s = list(colmaps = split(lvls, fcts))
+      # Extract names for new factor columns to be populated with lvls
+      matches = regmatches(cols, regexec(pv$group_pattern, cols, perl = TRUE))
+      # Error, if nothing was captured.
+      if (any(lengths(matches) < 2)) {
+        stopf("Pattern %s matches column name %s, but nothing was captured. Make sure group_pattern contains a capturing group.",
+              str_collapse(pv$group_pattern, quote = '"'),
+              str_collapse(cols[lengths(matches) < 2], quote = '"'))
+      }
+      fcts = map_chr(matches, 2)
 
+      # Error if no group could be extracted for an entry in col. Thus, we could not create a column name from it.
+      if (any(nchar(fcts) == 0)) {
+        stopf("Pattern %s with column(s) %s would produce empty string as decoded column name(s). Try using a different pattern.",
+              str_collapse(pv$group_pattern, quote = '"'),
+              str_collapse(cols[nchar(fcts) == 0], quote = '"'))
+      }
+
+      # Create mapping of old column names and derived levels to new column names
+      cmap = split(lvls, fcts)
       if (pv$treatment_encoding) {
-        # Set default name for reference level
-        ref_name = "ref"
-        counter = 1
-        while (ref_name %in% cols) {
-          ref_name = paste0("ref.", counter)
-          counter = counter + 1
-        }
-        # Append ref_name with empty name to all list entries
-        for (i in seq_along(s[["colmaps"]])) {
-          s[["colmaps"]][[i]][[length(s[["colmaps"]][[i]]) + 1]] = ref_name
+        # Append ref_name with empty name (i.e. "") to all list entries
+        for (map in cmap) {
+          map[[length(map) + 1]] = ref_name
         }
       }
 
-      s$cutoff = pv$treatment_cutoff %??% 0
-      s$ties_method = pv$ties_method
-      s
+      list(
+        colmaps = cmap,
+        treatment_encoding = pv$treatment_encoding,
+        cutoff = pv$treatment_cutoff %??% 0,
+        ties_method = pv$ties_method
+      )
     },
 
     .transform_dt = function(dt, levels) {
       colmaps = self$state$colmaps
+      # Early exit if no mapping is required
+      if (!length(colmaps)) {
+        return(dt)
+      }
       cutoff = self$state$cutoff
       ties_method = self$state$ties_method
+      treatment_encoding = self$state$treatment_encoding
 
-      for (fct in names(colmaps)) {
-        old_cols = names(colmaps[[fct]])
-        lvls = colmaps[[fct]]
+      for (new_col in names(colmaps)) {
+        lvls = colmaps[[new_col]]
+        old_cols = names(lvls)
 
+        # Create matrix from subset of dt with columns old_cols
         old_cols_matrix = as.matrix(dt[, old_cols, with = FALSE])
-
-        # Find the column with the maximal value for each row
-        set(dt, , fct, old_cols[apply(old_cols_matrix, 1, which_max, ties_method = ties_method)])
-
+        # Populate new column with name of column with maximal value per row
+        set(dt, , new_col, old_cols[apply(old_cols_matrix, 1, which_max, ties_method = ties_method)])
         # If any value in old_cols_matrix are smaller than the cutoff, replace with empty string
+        # This implies replacement with reference level in next step.
         if (treatment_encoding) {
           set(dt, rowSums(old_cols_matrix <= cutoff) > 0, "")
         }
-
-        # Assign the corresponding value from the named vector to the new column
-        set(dt, , fct, factor(lvls[match(dt[[fct]], names(lvls))], levels = lvls))
+        # Replace occurrences of old column names with corresponding new level names
+        set(dt, , new_col, factor(lvls[match(dt[[new_col]], names(lvls))], levels = lvls))
       }
 
       # Drop old columns
