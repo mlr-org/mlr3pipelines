f45e25d8acc4d86de1c9a5d7f17d92add4a48c80
kenomersmannPC <advieser@gmail.com>
2024-11-26 14:07:41 +0100

    added params and changed logic

--- R/PipeOpDecode.R
+++ R/PipeOpDecode.R
@@ -47,10 +47,12 @@ PipeOpDecode = R6Class("PipeOpDecode",
   public = list(
     initialize = function(id = "decode", param_vals = list()) {
       ps = ps(
-        treatment_encoding = p_lgl(tags = c("train", "predict")),
-        group_pattern = p_uty(custom_check = check_string, tags = c("train", "predict"))
+        treatment_encoding = p_lgl(tags = c("train", "required")),
+        treatment_cutoff = p_dbl(default = 0, tags = "train", requires = quote(treatment_encoding == TRUE)),
+        group_pattern = p_uty(custom_check = check_string, tags = c("train", "required")),
+        ties_method = p_fct(c("first", "last", "random"), tags = c("train", "required"))
       )
-      ps$values = list(treatment_encoding = FALSE, group_pattern = "^([^.]*)\\.")
+      ps$values = list(treatment_encoding = FALSE, group_pattern = "^([^.]+)\\.", ties_method = "random")
       super$initialize(id, param_set = ps, param_vals = param_vals, tags = "encode", feature_types = c("integer", "numeric"))
     }
   ),
@@ -60,22 +62,42 @@ PipeOpDecode = R6Class("PipeOpDecode",
       pv = self$param_set$values
       cols = colnames(dt)
 
+      if (treatment_encoding) {
+        # Determine name for reference level
+        ref_name = "ref"
+        counter = 1
+        while (ref_name %in% cols) {
+          ref_name = paste0("ref.", counter)
+          counter = counter + 1
+        }
+      }
+
       # If pattern == "", all columns are collapsed into one column
+      # If "pipeop.decoded" is already taken, we overwrite it!
       if (pv$group_pattern == "") {
-        return(list(colmaps = list(result = set_names(cols, cols))))
-        # should make sure that result is available (or name we chose instead)
+        if (treatment_encoding) {
+          # First add entry ref_name with empty name
+          cmap = list(pipeop.decoded = c(set_names(cols, cols)))
+          cmap[[pipeop.decoded]][[length(cols) + 1]] = ref_name
+          return(list(colmaps = cmap))
+        }
+        return(list(colmaps = list(pipeop.decoded = set_names(cols, cols))))
       }
 
       # Drop columns that do contain group_pattern
       # What about cols starting with .? -> probably let user exclude this by changing group_pattern are using affect_columns
-      cols = cols[grepl(pv$group_pattern, cols)]
+      cols = cols[grepl(pv$group_pattern, cols, perl = TRUE)]
 
       # Extract factor names
       # If group_pattern contains does not contain a capturing group, fcts cannot be generated
-      matches = regmatches(cols, regexec(pv$group_pattern, cols))
-      fcts = vapply(matches, function(x) x[[2]], character(1))
+      matches = regmatches(cols, regexec(pv$group_pattern, cols, perl = TRUE))
+      fcts = map_chr(matches, 2)
+
+      if (any(nchar(fcts) == 0)) {
+        stopf("Pattern %s with column %s would produce empty decoded column name", group_pattern, cols[nchar(fcts) == 0])
+      }
       # Extract level names
-      lvls = set_names(gsub(pv$group_pattern, "", cols), cols)
+      lvls = set_names(gsub(pv$group_pattern, "", cols, perl = TRUE), cols)
 
       # test that split is consistent for this use case
       s = list(colmaps = split(lvls, fcts))
@@ -94,25 +116,32 @@ PipeOpDecode = R6Class("PipeOpDecode",
         }
       }
 
+      s$cutoff = pv$treatment_cutoff %??% 0
+      s$ties_method = pv$ties_method
       s
     },
 
     .transform_dt = function(dt, levels) {
       colmaps = self$state$colmaps
+      cutoff = self$state$cutoff
+      ties_method = self$state$ties_method
 
       for (fct in names(colmaps)) {
         old_cols = names(colmaps[[fct]])
         lvls = colmaps[[fct]]
 
-        # Do we check that which.max is unique?
+        old_cols_matrix = as.matrix(dt[, old_cols, with = FALSE])
 
         # Find the column with the maximal value for each row
-        dt[, (fct) := old_cols[apply(.SD, 1, which.max)], .SDcols = old_cols]
-        # add handling for reference class if treatment_encoding == TRUE
+        set(dt, , fct, old_cols[apply(old_cols_matrix, 1, which_max, ties_method = ties_method)])
+
+        # If any value in old_cols_matrix are smaller than the cutoff, replace with empty string
+        if (treatment_encoding) {
+          set(dt, rowSums(old_cols_matrix <= cutoff) > 0, "")
+        }
 
         # Assign the corresponding value from the named vector to the new column
-        # dt[, (fct) := as.factor(lvls[get(fct)])]
-        dt[, (fct) := as.factor(lvls[dt[[fct]]])]
+        set(dt, , fct, factor(lvls[match(dt[[fct]], names(lvls))], levels = lvls))
       }
 
       # Drop old columns
