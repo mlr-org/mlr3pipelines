68360d7cd5040628cc8e8acf598c4c1798554fc9
kenomersmannLaptop <advieser@gmail.com>
2024-12-20 17:16:26 +0100

    small docs correction

--- R/PipeOpQuantileBin.R
+++ R/PipeOpQuantileBin.R
@@ -58,7 +58,7 @@ PipeOpQuantileBin = R6Class("PipeOpQuantileBin",
     initialize = function(id = "quantilebin", param_vals = list()) {
       ps = ps(
         numsplits = p_int(lower = 2, special_vals = list(NULL), tags = "train")
-        )
+      )
       ps$values = list(numsplits = 2L)
       super$initialize(id, param_set = ps, param_vals = param_vals, packages = "stats", feature_types = c("numeric", "integer"))
     }
