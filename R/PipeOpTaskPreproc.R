9840006c6d02adec3dc2efe37265f556e5d9c734
advieser <advieser@gmail.com>
2025-01-19 15:21:12 +0100

    fix for col with feature and other role in predict

--- R/PipeOpTaskPreproc.R
+++ R/PipeOpTaskPreproc.R
@@ -208,7 +208,7 @@ PipeOpTaskPreproc = R6Class("PipeOpTaskPreproc",
       if (do_subset) {
         affected_cols = self$param_set$values$affect_columns(intask)
         assert_subset(affected_cols, intask$feature_names, empty.ok = TRUE)
-        remove_cols = setdiff(intask$feature_names, affected_cols)
+        non_affected_cols = setdiff(intask$feature_names, affected_cols)
         intask$col_roles$feature = affected_cols
       }
       intasklayout = copy(intask$feature_types)
@@ -227,7 +227,7 @@ PipeOpTaskPreproc = R6Class("PipeOpTaskPreproc",
       }
 
       if (do_subset) {
-        intask$col_roles$feature = union(intask$col_roles$feature, y = remove_cols)
+        intask$col_roles$feature = union(intask$col_roles$feature, y = non_affected_cols)
       }
 
       list(intask)
@@ -238,9 +238,9 @@ PipeOpTaskPreproc = R6Class("PipeOpTaskPreproc",
       do_subset = !is.null(self$param_set$values$affect_columns)
 
       if (do_subset) {
-        # FIXME: see train fixme: this fails when something is both a feature and something else
-        remove_cols = setdiff(intask$feature_names, self$state$affected_cols)
-        intask$col_roles = map(intask$col_roles, .f = setdiff, y = remove_cols)
+        affected_cols = self$state$affected_cols
+        non_affected_cols = setdiff(intask$feature_names, affected_cols)
+        intask$col_roles$feature = affected_cols
       }
       if (!isTRUE(all.equal(self$state$intasklayout, intask$feature_types, ignore.row.order = TRUE))) {
         stopf("Input task during prediction of %s does not match input task during training.", self$id)
@@ -260,8 +260,7 @@ PipeOpTaskPreproc = R6Class("PipeOpTaskPreproc",
         stopf("Processed output task during prediction of %s does not match output task during training.", self$id)
       }
       if (do_subset) {
-        # FIXME: see train fixme: this fails if .train_task added a column with the same name
-        intask$col_roles$feature = union(intask$col_roles$feature, y = remove_cols)
+        intask$col_roles$feature = union(intask$col_roles$feature, y = non_affected_cols)
       }
       list(intask)
     },
