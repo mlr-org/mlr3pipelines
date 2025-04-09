5b23c375ab46b475d9e85f467a90d92fc6715ce6
kenomersmannLaptop <advieser@gmail.com>
2025-04-09 20:04:23 +0200

    WIP conceptual dev notes

--- R/preproc.R
+++ R/preproc.R
@@ -1,24 +1,28 @@
 #' @title Simple Preprocessing
 #'
 #' @description
+#'a
 #'
-#' calling train and predict with single_input = TRUE -> input task is copied and given to each unconnected input channel
-#'
-#'
-#' @param indata (`Task` | `data.frame`-like object)\cr
-#'   probably just write data.table. I think this is our convention.
-#'
-#'   If `indata` is a `data.frame`-like object, it is used to contruct a `TaskUnsupervised` internally.
-#' @param graph `Graph` | `PipeOp`\cr
+#' @param indata ([`Task`][mlr3::Task] | [`data.table`][data.table::data.table] )\cr
+#'   Data to be pre-processed. If `indata` is a `data.table`, it is used to
+#'   construct a [`TaskUnsupervised`][mlr3::TaskUnsupervised] internally, meaning
+#'   that it would not work with [`Graph`]s or [`PipeOp`]s that use the columns
+#'   of role `target`.
+#' @param graph ([`Graph`] | [`PipeOp`])\cr
 #'   modified-by-reference
-#' @param state named `list` | `NULL`\cr
+#'   could also be trained for predict (if state is given, original state gets overwirrten)
+#'   if trained and train is run, state gets overwritten
+#' @param state (named `list` | `NULL`)\cr
 #'   a
-#' @param predict `logical(1)`\cr
+#' @param predict (`logical(1)`)\cr
 #'   a
-#' @return description
+#' @return
 #' only single output channel
 #' - makes sense for piping and nature of this function to simplify things (i.e. should be used in simple context)
 #' - we couldn't identify the proper output channel (e.g. targetmutate)
+#'
+#' @section Internals
+#'
 #'
 #' @export
 #' @examples
@@ -31,36 +35,52 @@ preproc <- function(indata, graph, state = NULL, predict = !is.null(state)) {
   )
   assert_list(state, names = "unique", null.ok = TRUE)  # from Graph
   assert_flag(predict)
-
+  assert(
+    check_class(graph, "PipeOp"),
+    check_class(graph, "Graph")
+  )
   graph = as_graph(graph, clone = FALSE)
 
   if (nrow(graph$output) != 1) {
     stop("'graph' must have exactly one output channel.")
   }
-  # Check: graph accepts task_type in input (if not a task but a df, would need to add class of supervised; or do it later; potentially more expensive)
+  # Check: Graph must return a task (see discussion below @return)
 
   # Construct a Task from data.frame
   # TODO: Check what pos could potentially not handle targetless graphs
   #       can we test this here already?
-  #       probably all POs that don't allow Task, but TaskClassif or TaskRegr specifically?
+  #       probably all POs that don't allow Task, but TaskClassif or TaskRegr specifically? -> also learner
+  #       $task_type only exists for POTaskPreproc, or POImpute
+  #       graph does not have this ...
+  # Would be good to have a tag / property for PipeOp whether they work without targets
   if (is.data.frame(indata)) {
     task = TaskUnsupervised$new(id = "preproc_task", backend = indata)
   } else {
     task = indata
   }
+  # Check: graph accepts task$task_type in graph$input (if not a task but a df, would need to add class of supervised; or do it later; potentially more expensive)
+  # maybe not necessary since graph checks this itself
 
   if (predict) {
-    # if no state given and predict is true: predict might work
-    # do we need any checks on state?
-    graph$state = state
+    # Do we need any checks on state? Graph does most checks for us
+    # If a state is passed, we overwrite graph's state by assignment, should it already be trained
+    if (!is.null(state)) {
+      if (graph$is_trained) warning("'graph' is trained, but 'state' is explicitly given to preproc. Using passed 'state' for Prediction.")
+      graph$state = state
+    }
     outtask = graph$predict(task)[[1L]]
   } else if (is.null(state)) {
+    # If a trained graph is passed, we overwrite its state by re-training the graph.
+    if (graph$is_trained) warning("'graph' is trained, but preproc re-trains the Graph, overwriting its original state.")
     outtask = graph$train(task)[[1L]]
   } else {
-    stopf("Giving a state makes no sense if predict=FALSE ...")
+    stop("Inconsistent function arguments. 'predict' is given as 'FALSE' while 'state' is given as not-NULL.")
   }
 
+  # What if Graph has a PipeOpLearner in it? Return Prediction?
+  # Or do we want to force a return type Task above?
   if (is.data.frame(indata)) {
+    # Especially for df-input, PipeOpLearner in graph not possible
     outtask$data()
   } else {
     outtask
@@ -73,3 +93,23 @@ preproc <- function(indata, graph, state = NULL, predict = !is.null(state)) {
 #
 # preproc(tsk("iris"), graph = ppl("robustify"))
 # preproc(tsk("iris"), graph = ppl("robustify"), predict = TRUE)
+#
+filter = flt("mim")
+op = po("filter", filter, filter.nfeat = 2)
+preproc(tsk("iris"), op)
+preproc(tsk("iris"), flt("auc"))
+
+gr = po("smote") %>>% po("pca")
+preproc(tsk("iris"), gr)
+
+learner = lrn("regr.rpart")
+preproc(tsk("mtcars"), learner)
+# works but only returns NULL
+preproc(tsk("mtcars"), learner, state = state)
+# learner does not get trained here
+
+df = tsk("iris")$data(cols = task$feature_names)
+op = po("pca")
+preproc(df, op)
+state = list(pca = op$state)
+preproc(df, op, state)
--- tests/testthat/test_preproc.R
+++ tests/testthat/test_preproc.R
@@ -1,5 +1,35 @@
 context("preproc")
 
-test_that("preproc - ...", {
+test_that("preproc - basic functionality", {
+  task = tsk("iris")
+
+  # test that:
+  # General:
+  # - graph / POs get modified in-place
+  # With Task:
+  # - works with POs / Graphs
+  # - train and preproc with train return same thing
+  # - predict and preproc with predict return same thing
+  # - returns task
+  # With DF:
+  # - works with POs / Graphs
+  # - train and preproc with train return same thing
+  # - predict and preproc with predict return same thing
+  # - returns data.frame
+
+  # what po to use?
+  # what task to use?
+})
+
+test_that("preproc - error messages", {
+  # indata: Task or data.table-like object
+  # graph: only PipeOp or Graph (Filter or Learner not supported)
+  # predict: only logical
+  # state: list (but checks are mostly done in graph)
+  # graph has more than one output
+  # some error if graph cannot handle targetless task
+  # predict=FALSE while state is given
+  # state is given but graph is already trained (overwriting by assignment)
+  # state is not given but graph is trained (overwirting by re-training.)
 
 })
