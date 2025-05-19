324d30e46b61c88604418dca236a420cd1c8be36
advieser <advieser@gmail.com>
2025-05-19 21:05:23 +0200

    fixed GraphLearner tests

--- tests/testthat/test_GraphLearner.R
+++ tests/testthat/test_GraphLearner.R
@@ -10,6 +10,7 @@ test_that("basic graphlearner tests", {
 
   glrn = GraphLearner$new(gr)
   glrn$properties = setdiff(glrn$properties, "weights")  # FIXME: workaround until weights handling does not need to be part of the paramset
+  glrn$use_weights = "error"  # also need to update use_weights now
   expect_true(run_experiment(task, glrn)$ok)
   glrn$properties = c(glrn$properties, "weights")
 
@@ -40,8 +41,10 @@ test_that("basic graphlearner tests", {
   glrn2_clone = glrn2$clone(deep = TRUE)
   expect_learner(glrn2)
   glrn2$properties = setdiff(glrn2$properties, "weights")  # FIXME: see above
+  glrn2$use_weights = "error"  # see above
   expect_true(run_experiment(task, glrn2)$ok)
-  glrn2$properties = c(glrn2$properties, "weights")
+  glrn2$properties = c(glrn2$properties, "weights")  # reset changes
+  glrn2$use_weights = "use"  # reset changes
   glrn2$train(task)
   glrn2_clone$state = glrn2$state
 #  glrn2_clone$state$log = glrn2_clone$state$log$clone(deep = TRUE)  # FIXME: this can go when mlr-org/mlr3#343 is fixed
