22f4b77fa1ea88fdce2eee134a0aa5c97d5a8025
awinterstetter <A.Winterstetter@campus.lmu.de>
2025-07-06 09:44:52 +0200

    output test translated into a loop

--- tests/testthat/test_pipeop_info.R
+++ tests/testthat/test_pipeop_info.R
@@ -10,18 +10,50 @@ test_that("basic properties", {
   expect_pipeop_class(PipeOpInfo, list(id = "info"))
 })
 
+
+# Works with Task as input
 # expect_output
 test_that("output behavior is appropriate", {
   po_cat = PipeOpInfo$new(id = "info", log_target = "cat")
-  expect_output(poinfo_cat$train(list(tsk("iris"))))
+  expect_output(invisible(poinfo_cat$train(list(tsk("iris"))))) # expect output quite generic - is there a way to check cat() more specifically,
   po_warning = PipeOpInfo$new(id = "info", log_target = "warning")
-  expect_warning(po_warning$train(list(tsk("iris"))))
+  expect_warning(invisible(po_warning$train(list(tsk("iris")))))
   po_message = PipeOpInfo$new(id = "info", log_target = "message")
-  expect_message(po_message$train(list(tsk("iris"))))
+  expect_message(invisible(po_message$train(list(tsk("iris")))))
   po_none = PipeOpInfo$new(id = "info", log_target = "none")
-  expect_silent(po_none$train(list(tsk("iris")))) #passt das? -- nochmal nachgucken
+  expect_silent(invisible(po_none$train(list(tsk("iris"))))) # check it again
+
+  expect_output(poinfo_cat$predict(list(tsk("iris"))))
+  expect_warning(po_warning$predict(list(tsk("iris"))))
+  expect_message(po_message$predict(list(tsk("iris"))))
+  expect_silent(po_none$predict(list(tsk("iris"))))
 }
 )
-# kann man eventuell mit ner schleife zu kürzerem Code machen
+# could be condensed into a loop
+# formulate the same for $predict?
 
 
+for (i in c("cat", "warning", "message", "none")) {
+
+}
+test_that("output behavior is appropriate", {
+for (input in c(tsk("iris"), prediction)) {
+mapply(function(output, func_list) {
+  po = PipeOpInfo$new(id = "info", log_target = output)
+  func_list(po$train(list(input)))},
+  output = c("cat", "warning", "message", "none"),
+  func_list = list(expect_output, expect_warning, expect_message, expect_silent)
+)
+}})
+
+test_that("output behavior is appropriate", {
+  inputs = c(tsk("iris"), prediction)
+  lapply(inputs, function(inputs) {
+    mapply(function(output, func_list) {
+      po = PipeOpInfo$new(id = "info", log_target = output)
+      func_list(po$train(list(inputs)))},
+      output = c("cat", "warning", "message", "none"),
+      func_list = list(expect_output, expect_warning, expect_message, expect_silent)
+    )
+  })
+})
