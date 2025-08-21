42a3102c3c2fdde8ebefe8d24c0414ba1cb40cf8
Alexander Winterstetter <A.Winterstetter@campus.lmu.de>
2025-08-21 10:37:27 +0200

    test for isomap implemented

--- tests/testthat/test_pipeop_isomap.R
+++ tests/testthat/test_pipeop_isomap.R
@@ -9,7 +9,31 @@ test_that("PipeOpIsomap - basic properties", {
 
 test_that("compare to dimRed::isomap", {
   skip_if_not_installed("dimRed")
+  # Part 1 - Train-method
+  # Version 1 - PipeOpIsomap
+  task = tsk("iris")
+  po = po("isomap", knn = 50)
+  pipeop_result_train = po$train(list(task))[[1]]$data()
+  pipeop_iso_train = pipeop_result_train[, grepl("^iso \\d", colnames(pipeop_result_train)), with = FALSE]
+  pipeop_meta_train = pipeop_result_train[, !grepl("^iso \\d", colnames(pipeop_result_train)), with = FALSE]
+  # Version 2 - dimRed package
+  data = dimRed::loadDataSet("Iris")
+  dimRed_result_train = embed(data, "Isomap", knn = 50)
+  #dimRed_result_train@data@data = dimRed_result_train@data@data
 
+  expect_equal(as.matrix(pipeop_iso_train), dimRed_result_train@data@data)
+  expect_equal(as.data.frame(pipeop_meta_train), dimRed_result_train@data@meta)
+
+  # Part 2 - Predict-method
+  # Version 1
+  pipeop_result_predict = po$predict(list(task))[[1]]$data()
+  pipeop_iso_predict = pipeop_result_predict[, grepl("^iso \\d", colnames(pipeop_result_predict)), with = FALSE]
+  pipeop_meta_predict = pipeop_result_predict[, !grepl("^iso \\d", colnames(pipeop_result_predict)), with = FALSE]
+  # Version 2
+  dimRed_result_predict = predict(dimRed_result_train, data)
+
+  expect_equal(as.matrix(pipeop_iso_predict), dimRed_result_predict@data, tolerance = 0.001)
+  expect_equal(as.data.frame(pipeop_meta_predict), dimRed_result_predict@meta)
 })
 
 test_that("cannot handle missing values", {
@@ -27,6 +51,3 @@ test_that("cannot handle missing values", {
 #' emb2 <- predict(emb, dat)
 #' plot(emb)
 #' plot(emb2)
-
-dat = na.omit(penguins[,3:7])
-embed(dat, "Isomap", knn = 50)
