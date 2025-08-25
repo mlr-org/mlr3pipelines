014698d925c8b51152925a8edf28fb18f7e89c92
Alexander Winterstetter <A.Winterstetter@campus.lmu.de>
2025-08-25 21:20:25 +0200

    Fixes for hyperparameters get_geod and .mute

--- R/PipeOpIsomap.R
+++ R/PipeOpIsomap.R
@@ -10,14 +10,34 @@
 #' ```
 #' PipeOpIsomap$new(id = "isomap", ...)
 #' ```
+#'
 #' * `ìd` :: `character(1)`\cr
-#'   Identifier of resulting object, default "isomap"
+#'   Identifier of resulting object, default `"isomap"`
+#' * `param_vals` :: named `list`\cr
+#'   List of hyperparameter settings, overwriting the hyperparameter settings that would otherwise be set during construction. Default `list()`.
 #'
 #' @section Input and Output Channels:
+#' Input and output channels are inherited from [`PipeOpTaskPreproc`].
+#'
+#' The output is the input [`Task`][mlr3::Task] with the data projected on the lower dimension.
 #'
 #' @section State:
 #'
 #' @section Parameters:
+#' The parameters are the parameters inherited from [`PipeOpTaskPreproc`], as well as:
+#' * `knn` :: `numeric(1)`\cr
+#'   The number of nearest neighbors in the graph.
+#'   Initialized to 50.
+#' * `ndim` :: `numeric(1)`\cr
+#'   The number of embedding dimensions.
+#'   Initialized to 2.
+#' * `get_geod` :: `logical(1)`\cr
+#'   Determines whether the distance matrix should be kept in the `$state`
+#'   Initialized to `FALSE`.
+#' * `.mute` :: `character`\cr
+#'   A character vector containing the elements you want to mute (c("message", "output")).
+#'   Initialized to `character(0)`.
+#'
 #'
 #' @section Internals:
 #'
@@ -32,7 +52,6 @@
 #' @template seealso_pipeopslist
 #' @include PipeOpTaskPreproc.R
 #' @export
-#' @method predict Graph
 #'
 
 PipeOpIsomap = R6Class("PipeOpIsomap",
@@ -45,15 +64,15 @@ PipeOpIsomap = R6Class("PipeOpIsomap",
         get_geod = p_lgl(default = FALSE, tags = "train"),
         .mute = p_uty(tags = "train")
       )
-      ps$values = list(knn = 50, ndim = 2)
-      super$initialize(id = id, param_set = ps, param_vals = param_vals, feature_types = c("numeric", "integer"))
+      ps$values = list(knn = 50, ndim = 2, get_geod = FALSE)
+      super$initialize(id = id, param_set = ps, param_vals = param_vals, feature_types = c("numeric", "integer", "factor", "logical"))
     }
   ),
   private = list(
     .train_dt = function(dt, levels, target) {
-      #browser()
+      browser()
       pv = self$param_set$get_values(tags = "train")
-      embed_result = dimRed::embed(dt, "Isomap", knn = pv$knn, ndim = pv$ndim, .mute = NULL)
+      embed_result = dimRed::embed(dt, "Isomap", knn = pv$knn, ndim = pv$ndim, get_geod = pv$get_geod, .mute = NULL)
       self$state = list(embed_result = embed_result)
       embed_result@data@data
     },
@@ -65,7 +84,7 @@ PipeOpIsomap = R6Class("PipeOpIsomap",
 
 mlr_pipeops$add("isomap", PipeOpIsomap)
 
-# po = po("isomap", knn = 50, ndim = 2)
+# po = po("isomap", knn = 50, ndim = 2, get_geod = TRUE)
 
 # po$train(list(tsk("iris")))[[1]]$data()
 # po$predict(list(tsk("iris")))[[1]]$data()
