08f70e3cfc94979e7682f4460aaa364ed04219db
Alexander Winterstetter <A.Winterstetter@campus.lmu.de>
2025-08-26 19:56:33 +0200

    minor changes in tests isomap

--- tests/testthat/test_pipeop_isomap.R
+++ tests/testthat/test_pipeop_isomap.R
@@ -1,12 +1,19 @@
 context("PipeOpIsomap")
 
 test_that("PipeOpIsomap - basic properties", {
-  op = po("isomap")
+  op = po("isomap", .mute = "message")
   task = mlr_tasks$get("iris")
   #browser()
   expect_pipeop(op)
   expect_datapreproc_pipeop_class(PipeOpIsomap, task = task, tolerance = 1)
 })
+
+# Argumentation für tolerance = 1
+#' dat <- loadDataSet("Iris")
+#' emb <- embed(dat, "Isomap", knn = 50)
+#' emb2 <- predict(emb, dat)
+#' plot(emb)
+#' plot(emb2)
 
 test_that("compare to dimRed::isomap", {
   skip_if_not_installed("dimRed")
@@ -37,12 +44,6 @@ test_that("compare to dimRed::isomap", {
   expect_equal(as.data.frame(pipeop_meta_predict), dimRed_result_predict@meta)
 })
 
-# Argumentation für tolerance = 1
-#' dat <- loadDataSet("Iris")
-#' emb <- embed(dat, "Isomap", knn = 50)
-#' emb2 <- predict(emb, dat)
-#' plot(emb)
-#' plot(emb2)
 
 
 test_that("isomap algorithm requires data to be numeric", {
