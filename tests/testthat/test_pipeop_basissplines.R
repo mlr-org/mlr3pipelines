6f243fa6b777fd55bac3bc7949d70bad5eea5f95
Alexander Winterstetter <A.Winterstetter@campus.lmu.de>
2025-09-07 16:36:58 +0100

    added tests for PipeOpBasisSplines

---
+++ tests/testthat/test_pipeop_basissplines.R
@@ -0,0 +1,6 @@
+context("PipeOpBasisSplines")
+
+# test_that("PipeOpBasisSplines - basic properties", {
+#   task = mlr_tasks$get("mtcars")
+#   expect_datapreproc_pipeop_class(PipeOpBasisSplines, task = task)
+# })
