5674f4ee3686fae6d345fc55a2656b66704d1ce5
Alexander Winterstetter <A.Winterstetter@campus.lmu.de>
2025-10-20 16:48:43 +0200

    fixed error in test

--- tests/testthat/test_pipeop_classweights.R
+++ tests/testthat/test_pipeop_classweights.R
@@ -12,16 +12,15 @@ test_that("PipeOpClassWeights - basic properties", {
 })
 
 test_that("PipeOpClassWeights", {
-  op = PipeOpClassWeights$new()
+
+  op = po("classweights")
   task = mlr_tasks$get("pima")
 
-  op$param_set$values = list(minor_weight =  3)
+  op$param_set$values$minor_weight = 3
   nt = op$train(list(task))[[1L]]
   expect_equal(nt$data(), task$data())
-
   weights = if ("weights_learner" %in% names(nt)) "weights_learner" else "weights"
   expect_equal(nt[[weights]]$weight, ifelse(nt$truth(nt[[weights]]$row_ids) == "neg", 1, 3))
-
 })
 
 test_that("PipeOpClassWeights - error for Tasks without weights property, #937", {
