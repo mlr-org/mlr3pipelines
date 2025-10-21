0805eb605016d9641d9923df6bcdb0fa03229852
Alexander Winterstetter <A.Winterstetter@campus.lmu.de>
2025-10-21 15:14:01 +0200

    fixed documentation for PipeOpClassWeightsEx

--- R/PipeOpClassWeightsEx.R
+++ R/PipeOpClassWeightsEx.R
@@ -1,10 +1,67 @@
+#' @title Class Weights for Sample  - Extended
+#'
+#' @usage NULL
+#' @name mlr_pipeops_classweights
+#' @format [`R6Class`][R6::R6Class] object inheriting from [`PipeOpTaskPreproc`]/[`PipeOp`].
+#'
+#' @description
+#' Adds a class weight column to the [`Task`][mlr3::Task] that different [`Learner`][mlr3::Learner]s may be
+#' able to use for sample weighting. Sample weights are added to each sample according to the target class.
+#'
+#' Only binary [classification tasks][mlr3::TaskClassif] are supported.
+#'
+#' Caution: when constructed naively without parameter, the weights are all set to 1. The `minor_weight` parameter
+#' must be adjusted for this [`PipeOp`] to be useful.
+#'
+#' Note this only sets the `"weights_learner"` column.
+#' It therefore influences the behaviour of subsequent [`Learner`][mlr3::Learner]s, but does not influence resampling or evaluation metric weights.
+#'
+#' @section Construction:
+#' ```
+#' PipeOpClassWeights$new(id = "classweights", param_vals = list())
+#' ```
+#'
+#' * `id` :: `character(1)`
+#'   Identifier of the resulting  object, default `"classweights"`
+#' * `param_vals` :: named `list`\cr
+#'   List of hyperparameter settings, overwriting the hyperparameter settings that would otherwise be set during construction. Default `list()`.
+#'
+#' @section Input and Output Channels:
+#' Input and output channels are inherited from [`PipeOpTaskPreproc`]. Instead of a [`Task`][mlr3::Task], a
+#' [`TaskClassif`][mlr3::TaskClassif] is used as input and output during training and prediction.
+#'
+#' The output during training is the input [`Task`][mlr3::Task] with added weights column according to target class.
+#' The output during prediction is the unchanged input.
+#'
+#' @section State:
+#' The `$state` is a named `list` with the `$state` elements inherited from [`PipeOpTaskPreproc`].
+#'
+#' @section Parameters:
+#' The parameters are the parameters inherited from [`PipeOpTaskPreproc`]; however, the `affect_columns` parameter is *not* present. Further parameters are:
+#' * `minor_weight` :: `numeric(1)` \cr
+#'   Weight given to samples of the minor class. Major class samples have weight 1. Initialized to 1.
+#' @section Internals:
+#' Introduces, or overwrites, the "weights" column in the [`Task`][mlr3::Task]. However, the [`Learner`][mlr3::Learner] method needs to
+#' respect weights for this to have an effect.
+#'
+#' The newly introduced column is named `.WEIGHTS`; there will be a naming conflict if this column already exists and is *not* a
+#' weight column itself.
+#'
+#' @section Fields:
+#' Only fields inherited from [`PipeOp`].
+#'
+#' @section Methods:
+#' Only methods inherited from [`PipeOpTaskPreproc`]/[`PipeOp`].
+#'
+#' @family PipeOps
+#' @template seealso_pipeopslist
+#' @include PipeOpTaskPreproc.R
+#' @export
+#' @examples
 #' library("mlr3")
 #'
 #' task = tsk("spam")
-#' opb = po("classweightsex", param_vals = list(weight_method = "inverse class frequency"))
-#' opb = po("classweightsex", param_vals = list(weight_method = "inverse square root of frequency"))
-#' opb = po("classweightsex", param_vals = list(weight_method = "median frequency balancing"))
-#' opb = po("classweightsex", param_vals = list(weight_method = "explicit", mapping = c("setosa" = 0.3, "virginica" = 0.5, "versicolor" = 0.4)))
+#' opb = po("classweights")
 #'
 #' # task weights
 #' if ("weights_learner" %in% names(task)) {
@@ -85,3 +142,26 @@ PipeOpClassWeightsEx = R6Class("PipeOpClassWeightsEx",
 
 mlr_pipeops$add("classweightsex", PipeOpClassWeightsEx)
 
+# library("mlr3")
+#
+# task = tsk("spam")
+# opb = po("classweightsex", param_vals = list(weight_method = "inverse class frequency"))
+# opb = po("classweightsex", param_vals = list(weight_method = "inverse square root of frequency"))
+# opb = po("classweightsex", param_vals = list(weight_method = "median frequency balancing"))
+# opb = po("classweightsex", param_vals = list(weight_method = "explicit", mapping = c("setosa" = 0.3, "virginica" = 0.5, "versicolor" = 0.4)))
+#
+# task weights
+# if ("weights_learner" %in% names(task)) {
+#   task$weights_learner  # recent mlr3-versions
+# } else {
+#   task$weights  # old mlr3-versions
+# }
+#
+# double the instances in the minority class (spam)
+# opb$param_set$values$minor_weight = 2
+# result = opb$train(list(task))[[1L]]
+# if ("weights_learner" %in% names(result)) {
+#   result$weights_learner  # recent mlr3-versions
+# } else {
+#   result$weights  # old mlr3-versions
+# }
